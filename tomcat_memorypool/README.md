6c04a1527a7aad7fad22528b8830e0d6977b3d73
sriram-1550 <sriram.sekar>
2016-10-04 16:24:47 +0530

    Linux Server Monitoring | Plugin : Tomcat Memory | Tomcat Monitoring

---
+++ tomcat_memorypool/README.md
@@ -0,0 +1,55 @@
+Plugin for Tomcat Monitoring
+=============================
+
+For monitoring the performance metrics of your Tomcat server using Site24x7 Server Monitoring Plugins. 
+  
+
+PreRequisites
+======================
+
+Download tomcatMemorypool plugin from https://github.com/site24x7/server-plugins/tomcatMemorypool/tomcatMemorypool.py
+Place the plugin folder ‘tomcatMemorypool/tomcatMemorypool.py’ under agent plugins directory (/opt/site24x7/monagent/plugins/)
+
+
+Configure Tomcat to support statistics
+=======================================
+
+1. Edit your tomcat-users.xml file to provide access to /manager url 
+
+
+Configure the agent plugin
+==========================
+ 
+1. Now make the following changes in the tomcat plugin file ( copied to agent's plugin directory earlier ).
+ 
+	Replace the shebang character "#!" in line 1 to the appropriate path for python in your system. Eg : 
+		#!/usr/local/bin/python3
+
+2. You can configure the following for the plugin
+	TOMCAT_HOST = 'localhost'
+
+	TOMCAT_PORT = '8080'
+
+	TOMCAT_USERNAME = 'admin'
+
+	TOMCAT_PASSWORD = 'admin'
+
+	TOMCAT_URL = '/manager'
+
+	TOMCAT_CONNECTOR = 'http-nio-8080'
+
+	TOMCAT_TIMEOUT = '5'
+
+	 
+3. Save the changes and restart the agent.
+ 
+		/etc/init.d/site24x7monagent restart
+
+Site24x7 agent will now report tomcat statistics in the plugins tab under the site24x7.com portal.
+
+
+Tomcat Plugin Attributes:
+==========================
+
+Some of the collected tomcat attributes are as follows:
+usage_<memorypool_name> - usage stats of each memory pool
---
+++ tomcat_memorypool/tomcat_memorypool.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+"""
+
+Site24x7 Tomcat Plugin
+
+"""
+import argparse
+import sys
+import urllib2
+import socket
+import xml.etree.ElementTree as ET
+from math import log
+import json
+import os
+import traceback
+
+#if any impacting changes to this plugin kindly increment the plugin version here.
+PLUGIN_VERSION = "1"
+
+#Setting this to true will alert you when there is a communication problem while posting plugin data to server
+HEARTBEAT="true"
+
+#Config Section:
+TOMCAT_HOST = 'localhost'
+
+TOMCAT_PORT = '8080'
+
+TOMCAT_USERNAME = 'admin'
+
+TOMCAT_PASSWORD = 'admin'
+
+TOMCAT_URL = '/manager'
+
+TOMCAT_CONNECTOR = None
+
+TOMCAT_TIMEOUT = '5'
+
+class Tomcat(object):
+    
+    def __init__(self,config):
+        self.configurations = config
+        self.host = self.configurations.get('host', 'localhost')
+        self.port = int(self.configurations.get('port', '8080'))
+        self.username = self.configurations.get('user')
+        self.password = self.configurations.get('password')
+        self.connector = self.configurations.get('connector')
+        self.url = self.configurations.get('url','/manager')
+        self.timeout = int(self.configurations.get('timeout'))
+        
+    def readXmlFromUrl(self,host,port,url,user,password):
+        xmlUrl = url+"/status?XML=true"
+        xmlData,xmlError = self.readUrl(host,port,xmlUrl,user,password)
+        if(xmlError):
+            return xmlData,xmlError
+        else:
+            try:
+                root = ET.fromstring(xmlData)
+            except ET.ParseError as e:
+                root="ERROR: Unable to read the XML page. Error: %s" %(e)
+                xmlError=True
+            return root,xmlError
+        
+    def readUrl(self,host,port,url,user,password):
+        error=False
+        tomcatUrl = "http://"+host+":"+str(port)+url
+        try:
+            pwdManager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            pwdManager.add_password(None,tomcatUrl,user,password)
+            authHandler = urllib2.HTTPBasicAuthHandler(pwdManager)
+            opener=urllib2.build_opener(authHandler)
+            urllib2.install_opener(opener)
+            req = urllib2.Request(tomcatUrl)
+            handle = urllib2.urlopen(req, None)
+            data = handle.read()
+        except urllib2.HTTPError as e:
+            if(e.code==401):
+                data="ERROR: Unauthorized user. Does not have permissions. %s" %(e)
+            elif(e.code==403):
+                data="ERROR: Forbidden, yours credentials are not correct. %s" %(e)
+            else:
+                data="ERROR: The server couldn\'t fulfill the request. %s" %(e)
+            error=True
+        except urllib2.URLError as e:
+            data = 'ERROR: We failed to reach a server. Reason: %s' %(e.reason)
+            error = True
+        except socket.timeout as e:
+            data = 'ERROR: Timeout error'
+            error = True
+        except socket.error as e:
+            data = "ERROR: Unable to connect with host "+self.host+":"+self.port
+            error = True
+        except:
+            data = "ERROR: Unexpected error: %s"%(sys.exc_info()[0])
+            error = True
+
+        return data,error
+
+        
+    def metricCollector(self):
+        data = {}
+        data['plugin_version'] = PLUGIN_VERSION
+        data['heartbeat_required']=HEARTBEAT
+        
+        socket.setdefaulttimeout(float(self.timeout))
+        serverinfoUrl = self.url+"/serverinfo"
+        serverinfoData,serverinfoError = self.readUrl(self.host,self.port,serverinfoUrl,self.username,self.password)
+        if serverinfoError:
+            serverinfoUrl = self.url+"/text/serverinfo"
+            serverinfoData,serverinfoError = self.readUrl(self.host,self.port,serverinfoUrl,self.username,self.password)
+        if(serverinfoError==False):
+            serverinfo = serverinfoData.splitlines()
+            tomcatVersionStr = (serverinfo[1].split(":"))[1]
+            tomcatStatusStr = serverinfo[0]
+            tomcatVersion = (tomcatVersionStr.split("/"))[1].split(".")[0]
+            if (tomcatVersion.isdigit()):
+                data['tomcat_version']=tomcatVersion
+            if(tomcatStatusStr.split(' ')[0]=='OK'):
+                data['tomcat_server_status']=1
+            else:
+                data['tomcat_server_status']=0
+            xmlTreeData,xmlError = self.readXmlFromUrl(self.host,self.port,self.url,self.username,self.password)
+            if (xmlError!=True):
+                if (xmlTreeData!=None):
+                    if data['tomcat_server_status']==0:
+                        if xmlTreeData.tag=='status':   
+                            data['tomcat_server_status']=1
+                    for mempool in xmlTreeData.findall('.//memorypool'):
+                        name=str(mempool.get('name'))
+                        name=name.replace(" ", "_")
+                        data['usage_'+name]=float(mempool.get('usageUsed'))       
+                else:
+                    data['msg']='Unable to collect data for the server'    
+        else:
+            data['msg']=serverinfoData 
+            data['tomcat_server_status']=0       
+        return data
+
+
+if __name__ == "__main__":
+
+    configurations = {'host': TOMCAT_HOST,'port': TOMCAT_PORT,'user': TOMCAT_USERNAME,'password': TOMCAT_PASSWORD, 'url': TOMCAT_URL, 'connector': TOMCAT_CONNECTOR, 'timeout': TOMCAT_TIMEOUT}
+
+    tomcat_plugins = Tomcat(configurations)
+    
+    result = tomcat_plugins.metricCollector()
+    
+    print(json.dumps(result, indent=4, sort_keys=True))
