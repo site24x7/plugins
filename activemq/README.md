68746fd2d340be3a186aedfeaa43aa51f70cb1f9
sriram-1550 <sriram.sekar>
2016-10-04 15:59:39 +0530

    Linux Server Monitoring | Plugin : ActiveMQ | ActiveMQ Monitoring

---
+++ activemq/README.md
@@ -0,0 +1,38 @@
+
+Plugin for Apache ActiveMQ Monitoring
+=====================================
+
+For monitoring the performance metrics of your ActiveMQ setup using Site24x7 Server Monitoring Plugins. 
+  
+
+Prerequisites
+=============
+
+Download activemq plugin from https://github.com/site24x7/plugins/activemq/activemq.py
+Place the plugin folder 'activemq/activemq.py' under agent plugins directory (/opt/site24x7/monagent/plugins/)
+
+
+Configure the agent plugin
+==========================
+ 
+1. Make the following changes in the activemq plugin file ( copied to agent's plugin directory earlier ).
+ 
+	Change the values of ACTIVEMQ_HOST, ACTIVEMQ_PORT, ACTIVEMQ_USERNAME and ACTIVEMQ_PASSWORD to match your configuration.
+ 
+2. Save the changes and restart the agent.
+ 
+		/etc/init.d/site24x7monagent restart
+
+Site24x7 agent will now report ActiveMQ statistics in the plugins tab under the site24x7.com portal.
+
+
+ActiveMQ Plugin Attributes:
+===========================
+
+Some of the collected ActiveMQ attributes are as follows:
+
+		"total_message_count" : Total number of messages in queue
+		"total_connections_count" : Total number of connections.
+		"total_consumer_count" : Total number of consumers.
+		"total_producer_count" : Total number of producers.
+
---
+++ activemq/activemq.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+"""
+
+Site24x7 ActiveMQ Plugin
+
+"""
+
+import json
+
+# if any impacting changes to this plugin kindly increment the plugin
+# version here.
+PLUGIN_VERSION = "1"
+
+# Setting this to true will alert you when there is a communication
+# problem while posting plugin data to server
+HEARTBEAT = "true"
+
+# Config Section:
+ACTIVEMQ_HOST = "localhost"
+
+ACTIVEMQ_PORT = "8161"
+
+ACTIVEMQ_USERNAME = "admin"
+
+ACTIVEMQ_PASSWORD = "admin"
+
+# Mention the units of your metrics . If any new metrics are added, make
+# an entry here for its unit if needed.
+METRICS_UNITS = {'total_message_count': 'units', 'total_connections_count': 'units',
+                 'total_consumer_count': 'units', 'total_producer_count': 'units'}
+
+
+class ActiveMQ(object):
+    def __init__(self, config):
+        self.configurations = config
+        self.connection = None
+        self.host = self.configurations.get('host', 'localhost')
+        self.port = int(self.configurations.get('port', '8161'))
+        self.username = self.configurations.get('user', 'root')
+        self.password = self.configurations.get('password', '')
+
+    def checkPreRequisites(self, data):
+        bool_result = True
+        try:
+            import requests
+        except Exception:
+            data['status'] = 0
+            data['msg'] = 'requests module not installed'
+            bool_result = False
+            requests_returnVal = os.system(
+                'pip install requests >/dev/null 2>&1')
+            if requests_returnVal == 0:
+                bool_result = True
+                data.pop('status')
+                data.pop('msg')
+        return bool_result, data
+
+    def metricCollector(self):
+        data = {}
+        data['plugin_version'] = PLUGIN_VERSION
+        data['heartbeat_required'] = HEARTBEAT
+
+        bool_result, data = self.checkPreRequisites(data)
+
+        if bool_result == False:
+            return data
+        else:
+            try:
+                import requests
+                from requests import ConnectionError
+            except Exception:
+                data['status'] = 0
+                data['msg'] = 'requests module not installed'
+                return data
+
+            url = 'http://%s:%s/api/jolokia/read/org.apache.activemq:type=Broker,brokerName=localhost' % (
+                self.host, self.port)
+
+            try:
+                r = requests.get(url, auth=(self.username, self.password))
+                json_data = r.json()
+
+                total_message_count = json_data['value']['TotalMessageCount']
+                total_connections_count = json_data[
+                    'value']['TotalConnectionsCount']
+                total_consumer_count = json_data['value']['TotalConsumerCount']
+                total_producer_count = json_data['value']['TotalProducerCount']
+
+                data['total_message_count'] = total_message_count
+                data['total_connections_count'] = total_connections_count
+                data['total_consumer_count'] = total_consumer_count
+                data['total_producer_count'] = total_producer_count
+            except ConnectionError as e:
+                data['status'] = 0
+                data['msg'] = str("Connection error.")
+
+            data['units'] = METRICS_UNITS
+
+        return data
+
+
+if __name__ == "__main__":
+    configurations = {'host': ACTIVEMQ_HOST, 'port': ACTIVEMQ_PORT,
+                      'user': ACTIVEMQ_USERNAME, 'password': ACTIVEMQ_PASSWORD}
+
+    activemq_plugins = ActiveMQ(configurations)
+
+    result = activemq_plugins.metricCollector()
+
+    print(json.dumps(result, indent=4, sort_keys=True))
