8534c645c6d3b4871f448c82ebf76f34798d0ee2
site24x7 <site24x7@users.noreply.github.com>
2017-01-11 18:57:47 +0530

    request module dependency removed instead urllib is used

--- couchserver/couchserver.py
+++ couchserver/couchserver.py
@@ -2,6 +2,8 @@
 
 import json
 
+import sys
+
 #if any changes to this plugin kindly increment the plugin version here.
 PLUGIN_VERSION=1
 
@@ -19,50 +21,45 @@ COUCHBASE_SERVER_USERNAME=None
 
 COUCHBASE_SERVER_PASSWORD=None
 
+REALM=None
+
 DEFAULT_TIMEOUT=30
 
 METRICS_UNITS = {'hdd.ram':'MB',"hdd.quotaTotal": "MB", "hdd.total": "MB","hdd.used": "MB","hdd.usedByData": "MB","ram.quotaTotalPerNode": "MB","ram.quotaUsed": "MB","ram.quotaUsedPerNode": "MB","ram.total":"MB","ram.used": "MB"}
 
+PYTHON_MAJOR_VERSION = sys.version_info[0]
+
+if PYTHON_MAJOR_VERSION == 3:
+    import urllib
+    import urllib.request as urlconnection
+elif PYTHON_MAJOR_VERSION == 2:
+    import urllib2 as urlconnection
+
 def metricCollector():
 	data = {}
+	
 	#defaults
-
 	data['plugin_version'] = PLUGIN_VERSION
 
 	data['heartbeat_required']=HEARTBEAT
 
 	data['units']=METRICS_UNITS
 
-	import requests
+	URL = "http://"+COUCHBASE_SERVER_HOST+":"+COUCHBASE_SERVER_PORT+"/"+COUCHBASE_SERVER_STATS_URI
 	
-	from requests.exceptions import ConnectionError
-	
-	server_url = "http://"+COUCHBASE_SERVER_HOST+":"+COUCHBASE_SERVER_PORT+"/"+COUCHBASE_SERVER_STATS_URI
-	
-	credentials = None
-
-	if COUCHBASE_SERVER_USERNAME and COUCHBASE_SERVER_PASSWORD:
-		credentials = (COUCHBASE_SERVER_USERNAME,COUCHBASE_SERVER_PASSWORD)
-
-	req_headers={}
-
-	req_headers['Accept'] = 'text/json'
-
 	try:
-		response = requests.get(server_url, auth=credentials, headers=req_headers,timeout=DEFAULT_TIMEOUT)
-		if response.status_code == 404:
-			data['status']=0
-			data['msg']='received status code 404'
-			return data
-		if response.status_code == 400:
-			data['status']=0
-			data['msg']='received status code 400'
-			return data
-		if response.status_code == 403:
-			data['status']=0
-			data['msg']='received status code 403'
-			return data
-		couchserver_dict = response.json()
+		
+		if COUCHBASE_SERVER_USERNAME and COUCHBASE_SERVER_PASSWORD:
+		   password_mgr = urlconnection.HTTPPasswordMgr()
+		   password_mgr.add_password(REALM, URL, COUCHBASE_SERVER_USERNAME, COUCHBASE_SERVER_PASSWORD)
+		   auth_handler = urlconnection.HTTPBasicAuthHandler(password_mgr)
+		   opener = urlconnection.build_opener(auth_handler)
+		   urlconnection.install_opener(opener)
+		response = urlconnection.urlopen(URL, timeout=10)
+		byte_responseData = response.read()
+		str_responseData = byte_responseData.decode('UTF-8')
+
+		couchserver_dict = json.loads(str_responseData)
 		data['hdd.total']=couchserver_dict['storageTotals']['hdd']['total']
 		data['hdd.quotaTotal']=couchserver_dict['storageTotals']['hdd']['quotaTotal']
 		data['hdd.usedByData']=couchserver_dict['storageTotals']['hdd']['usedByData']
@@ -77,9 +74,9 @@ def metricCollector():
 			if '.' in item:
 				data[item]=convertBytesToMB(data[item])	
 		
-	except ConnectionError as e:
+	except Exception as e:
 			data['status']=0
-			data['msg']='Connection Error'  
+			data['msg']=str(e)
 	
 	return data
 
