a0b84b6270269c59139a809915429d9c01e7fc4b
godwyn.j <godwyn.j@zohocorp.com>
2017-10-13 14:43:04 +0530

    Oracle plugin Monitor

---
+++ oracle/README.md
@@ -0,0 +1,77 @@
+Plugin for Oracle DB Monitoring
+=====================================
+
+For monitoring the performance metrics of your Oracle DB using Site24x7 Server Monitoring Plugins. Make sure you have the Site24x7 Linux server agent installed to add this plugin and monitor your Oracle database.
+  
+
+Prerequisites
+=============
+
+
+Ensure the following modules are installed to connect to the Oracle database:
+    
+    1. 	oracle-instantclient-basic-linux
+	
+    2.	oracle-instantclient-sdk-linux
+	
+    3.  cx_Oracle - python module
+	
+	In case you do not have the Oracle instant client installed already, you can install it by following the steps below:
+	#Installing and configuring Oracle instant client
+	================================================
+	1. Dependencies
+	Install the following packages
+		apt-get install python-dev build-essential libaio1
+	2. Download instant client for Linux x86-64 
+		instantclient-basic-linux.x64-12.2.0.1.0.zip
+		instantclient-sdk-linux.x64-12.2.0.1.0.zip  
+	from Oracle Website (link : http://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html ). Please sign in to download
+	3. Unzip and extract the downloaded zip files into a folder, and extract the zip files there.
+	   for example, in a directory 
+		mkdir -p /opt/oracle_client 
+		
+	4.Add environment variables:
+   	   Create a file in /etc/profile.d/oracle.sh and add the following lines
+		export ORACLE_HOME=/opt/oracle_client/instantclient_12_2
+		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
+		
+	   Create a file in /etc/ld.so.conf.d/oracle.conf  and add the following lines
+		/opt/oracle_client/instantclient_12_2
+	   	
+	   Execute the command
+		sudo ldconfig
+		
+		**Note: You need to Log out and log in,after this step for the changes to reflect**
+
+	5. Create a symlink 
+		cd $ORACLE_HOME
+		ln -s libclntsh.so.12.1 libclntsh.so
+		
+	6. Install cx_oracle python using pip
+		pip install cx_Oracle
+
+Configure the agent plugin
+==========================
+ 
+1. Make the following changes in the oracle.py plugin file:
+           
+         Change the values of ORACLE_HOST, ORACLE_PORT, ORACLE_USERNAME, ORACLE_PASSWORD and ORACLE_SID to match your configuration.
+2. To add the oracle.py plugin to the Site24x7 Linux agent, 
+       
+         1) Go to the agent's installation directory
+                cd /opt/site24x7/monagent/plugins/
+         2) Create a folder named oracle
+                mkdir oracle
+         3) Copy the plugin to this folder. (Note: The folder name and the plugin name should be the same)
+3. The plugin will be added to Site24x7 automatically. This may take less than five minutes.
+
+
+#Metrics Collected
+===================
+		*buffer_cache_hit_ratio
+		*disk_memory_sort_ratio
+		*oracle_status  - up=1 and down = 0
+		*processes_usage % 
+		*response_time ms
+		*sessions_usage %
+
---
+++ oracle/oracle.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+"""
+
+Site24x7 Oracle DB Plugin
+
+"""
+
+import json
+# if any changes are done to the metrics or units, increment the plugin version number here 
+PLUGIN_VERSION = "1"
+
+# Setting this to true will alert you when plugin is down
+HEARTBEAT = "true"
+
+# Config Section: Enter your configuration details to connect to the oracle database
+ORACLE_HOST = "site24x7-win8"
+
+ORACLE_PORT = "1521"
+
+ORACLE_SID = "XE"
+
+ORACLE_USERNAME = "sys"
+
+ORACLE_PASSWORD = "test"
+
+# Mention the units of your metrics . If any new metrics are added, make an entry here for its unit if needed.
+METRICS_UNITS = {'processes_usage': '%','sessions_usage':'%','response_time':'ms'}
+#oracle_status = 1 denotes UP and 0 denotes down
+
+class Oracle(object):
+    def __init__(self, config):
+        self.configurations = config
+        self.connection = None
+        self.host = self.configurations.get('host', 'localhost')
+        self.port = int(self.configurations.get('port', '1521'))
+        self.sid = self.configurations.get('sid', 'XE')
+        self.username = self.configurations.get('user', 'sys')
+        self.password = self.configurations.get('password', 'admin')
+        self.data = {'plugin_version': PLUGIN_VERSION, 'heartbeat_required': HEARTBEAT, 'units':METRICS_UNITS}
+     
+    def metricCollector(self):
+        c=None
+        conn=None
+        try:
+            import cx_Oracle
+        except Exception as e:
+            self.data['status'] = 0
+            self.data['msg'] = str(e)
+            return self.data
+
+        try:
+            try:
+                dsnStr = cx_Oracle.makedsn(self.host, self.port, self.sid)
+                conn = cx_Oracle.connect(user=self.username, password=self.password, dsn=dsnStr, mode=cx_Oracle.SYSDBA)
+                c = conn.cursor()
+            except Exception as e:
+                self.data['status']=0
+                self.data['msg']='Exception while connecting to '+self.host
+                return self.data
+                
+            c.execute("SELECT  status  FROM v$instance")
+            for row in c:
+                status = row[0]
+                self.data['oracle_status'] = 1 if status == 'OPEN' else 0
+                break
+            
+            c.execute("SELECT resource_name name, 100*DECODE(initial_allocation, ' UNLIMITED', 0, current_utilization / initial_allocation) usage FROM v$resource_limit WHERE LTRIM(limit_value) != '0' AND LTRIM(initial_allocation) != '0' AND resource_name in('sessions','processes')")
+            for row in c:
+                resource,usage = row
+                self.data[resource+'_usage']= round(usage,2)
+            
+            c.execute("SELECT to_char(begin_time, 'hh24:mi'),round(value * 10, 2) FROM v$sysmetric WHERE metric_name = 'SQL Service Response Time'")
+            for row in c:
+                self.data['response_time'] = float(row[1])
+                break
+               
+            # Disk to memory sort ratio
+			# http://oln.oracle.com/DBA/OLN_OPTIMIZING_SORTS/sorts/html/lesson2/124_01a.htm
+
+            c.execute("SELECT disk.value, mem.value,(disk.value / mem.value) * 100  FROM v$sysstat mem, v$sysstat disk WHERE mem.name = 'sorts (memory)' AND disk.name = 'sorts (disk)'")
+
+            for row in c:
+                disk_sort, memory_sort, ratio = row
+                self.data['disk_memory_sort_ratio'] = ratio
+                
+            # Calculate buffer cache hit ratio
+            # https://docs.oracle.com/database/121/TGDBA/tune_buffer_cache.htm#TGDBA533
+
+            c.execute(
+                "SELECT name, value FROM V$SYSSTAT "
+                "WHERE name IN ('db block gets from cache', 'consistent gets from cache', 'physical reads cache')")
+
+            for row in c:
+                name, value = row
+                if name == 'db block gets from cache':
+                    db_blocks = value
+                if name == 'consistent gets from cache':
+                    consistent_gets = value
+                if name == 'physical reads cache':
+                    physical_reads = value
+
+            buffer_cache_hit_ratio = 1 - (physical_reads / float(consistent_gets + db_blocks))
+            self.data['buffer_cache_hit_ratio'] = round(buffer_cache_hit_ratio,2)  # formatting to two decimals
+
+        except Exception as e:
+            self.data['status'] = 0
+            self.data['msg'] = str(e)
+        finally:
+            if c!= None : c.close()
+            if conn != None : conn.close()
+            return self.data
+
+if __name__ == "__main__":
+    configurations = {'host': ORACLE_HOST, 'port': ORACLE_PORT,
+                      'user': ORACLE_USERNAME, 'password': ORACLE_PASSWORD, 'sid': ORACLE_SID}
+
+    oracle_plugin = Oracle(configurations)
+
+    result = oracle_plugin.metricCollector()
+
+print(json.dumps(result, indent=4, sort_keys=True))
