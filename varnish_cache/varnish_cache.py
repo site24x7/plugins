88ac78d5c456b870e1cdd1165c660ec03e96da74
site24x7 <site24x7@users.noreply.github.com>
2018-12-21 17:03:01 +0530

    Multiple instance support added for varnish cache

--- varnish_cache/varnish_cache.py
+++ varnish_cache/varnish_cache.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 """
 Site24x7 Varnish Plugin
 
@@ -16,8 +17,7 @@ PLUGIN_VERSION = "1"
 HEARTBEAT = "true"
 
 # Config Section:
-VARNISH_HOST = "localhost"
-VARNISH_PORT = "6082"
+VARNISH_INSTANCE = ""
 
 # Mention the units of your metrics . If any new metrics are added, make an entry here for its unit if needed.
 METRICS_UNITS_V3 = {'cache_hit': 'units', 'cache_miss': 'units', 'n_wrk_create': 'units', 'n_wrk_lqueue': 'units',
@@ -30,12 +30,19 @@ class VarnishCache(object):
     def __init__(self, config):
         self.configurations = config
         self.connection = None
-        self.host = self.configurations.get('host', 'localhost')
-        self.port = int(self.configurations.get('port', '6082'))
 
     def metric_collector(self):
-        data = {'plugin_version': PLUGIN_VERSION, 'heartbeat_required': HEARTBEAT}
-        output = subprocess.check_output(['varnishstat', '-1', '-j'])
+        data = {
+            'plugin_version': PLUGIN_VERSION, 'heartbeat_required': HEARTBEAT
+        }
+	    
+        varnish_command = ['varnishstat', '-1', '-j']
+
+        if VARNISH_INSTANCE != "":
+	        varnish_command.append('-n')
+	        varnish_command.append(VARNISH_INSTANCE)	
+
+        output = subprocess.check_output(varnish_command)
         j = json.loads(output.decode())
         varnish_version = self.get_major_version()
         if int(varnish_version) < 4:
@@ -59,7 +66,7 @@ class VarnishCache(object):
         return version.group(1)[0]
 
 if __name__ == "__main__":
-    configurations = {'host': VARNISH_HOST, 'port': VARNISH_PORT}
+    configurations = {}
     varnish_plugins = VarnishCache(configurations)
     result = varnish_plugins.metric_collector()
     print(json.dumps(result, indent=4, sort_keys=True))
