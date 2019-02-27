c64560eba12ff8659023582319f78de7e804900c
site24x7 <site24x7@users.noreply.github.com>
2019-02-27 12:01:54 +0530

    New attributes added
    hit ratio, total_keys, used_memory, incoming traffic , outgoing traffic.

--- redis/Redis.py
+++ redis/Redis.py
@@ -5,7 +5,7 @@
   
 """
 
-import json
+import json,sys
 
 
 #if any impacting changes to this plugin kindly increment the plugin version here.
@@ -51,11 +51,11 @@ METRICS = {
     "evicted_keys": "evicted keys", 
     "expired_keys": "expired keys", 
     #"hz": "hz", 
-    #"instantaneous_input_kbps": "instantaneous input kbps", 
-    #"instantaneous_ops_per_sec": "instantaneous ops per sec", 
-    #"instantaneous_output_kbps": "instantaneous output kbps", 
-    #"keyspace_hits": "keyspace hits", 
-    #"keyspace_misses": "keyspace misses", 
+    "instantaneous_input_kbps": "incoming traffic", 
+    "instantaneous_ops_per_sec": "ops/sec",
+    "instantaneous_output_kbps": "output traffic", 
+    "keyspace_hits": "keyspace hits", 
+    "keyspace_misses": "keyspace misses", 
     #"latest_fork_usec": "latest fork usec", 
     #"loading": "loading", 
     #"lru_clock": "lru clock", 
@@ -91,23 +91,21 @@ METRICS = {
     #"total_net_output_bytes": "total net output bytes", 
     #"uptime_in_days": "uptime in days", 
     "uptime_in_seconds": "uptime", 
-    #"used_cpu_sys": "used cpu sys", 
+    "used_cpu_sys": "used cpu sys", 
     #"used_cpu_sys_children": "used cpu sys children", 
-    #"used_cpu_user": "used cpu user", 
+    "used_cpu_user": "used cpu user", 
     #"used_cpu_user_children": "used cpu user children", 
     "used_memory": "used memory", 
     #"used_memory_human": "used memory human", 
     #"used_memory_lua": "used memory lua", 
     #"used_memory_peak": "used memory peak", 
-    "used_memory_peak_human": "used memory peak human", 
+    #"used_memory_peak_human": "used memory peak human", 
     #"used_memory_rss": "used memory rss"
 }
 
 
 #Mention the units of your metrics in this dictionary. If any new metrics are added make an entry here for its unit.
-METRICS_UNITS={'instantaneous_input_kbps':'kbps','instantaneous_output_kbps':'kbps',
-               'total_net_input_bytes':'bytes','total_net_output_bytes':'bytes'
-              }
+METRICS_UNITS={'incoming traffic':'kbps','outgoing traffic':'kbps','used memory':'KB'}
 
 class Redis(object):
     def __init__(self,config):
@@ -129,7 +127,6 @@ class Redis(object):
             data['status']=0
             data['msg']='Redis Module Not Installed'
             return data
-            
         stats = None
         for db in self.dbs.split(','):
             try:
@@ -137,31 +134,37 @@ class Redis(object):
                 stats = redis_connection.info()
             except Exception as e:
                 data['status']=0
-                data['msg']='Connection Error'
-
+                data['msg']=str(e)
         if not stats:
             return data
-
         #stats.items() for python version greater than 3 otherwise stats.iteritems()
+        total_keys=0
         for name, value in stats.items():
-            try:
+            if type(value)==dict:
+               total_keys+=value['keys']
+	    try:
                 if name in METRICS.keys() :
-                    if METRICS[name] in ['used_memory_peak_human', 'used_memory_human']:
-                        value = float(value[0:-1])
+                    if METRICS[name] == 'used memory':
+		       value = value / 1024
                     data[METRICS[name]] = value
             except (ValueError, TypeError) as e:
-                #print('error -- {0}'.format(name))
                 data[name] = value
-
-        
-        
+        total_key_stats = data['keyspace hits'] + data['keyspace misses']
+	if total_key_stats == 0:
+           data['hit ratio']=0
+	else:
+	   data['hit ratio'] = data['keyspace hits'] / total_key_stats
+	data['total_keys']=total_keys
         data['units']=METRICS_UNITS
-        
         return data
 
 
 if __name__ == '__main__':
     
+    if len(sys.argv) > 1:
+        REDIS_USERNAME = sys.argv[1]
+        REDIS_PASSWORD = sys.argv[2]
+
     configuration = {'host': REDIS_HOST,'port': REDIS_PORT,'dbs': REDIS_DBS,'password': REDIS_PASSWORD,'queues': REDIS_QUEUES}
 
     redis_plugin = Redis(configuration)
