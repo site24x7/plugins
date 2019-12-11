b4b4b7e8ee22060e333b39bbc94fd2e13c06bae4
site24x7 <site24x7@users.noreply.github.com>
2019-12-11 15:38:46 +0530

    Windows power shell plugin

---
+++ idrac_sensor/idrac_sensor.ps1
@@ -0,0 +1,19 @@
+﻿$version = "1"
+
+$displayname = "idrac_sensor"
+
+$heartbeat = "False"
+
+$python = "<python-installed-path>\Python36-32\python.exe"  #change the pytho path
+$plugin = "idrac_sensor.py"
+$data = & $python $plugin
+$dataHash = $data | ConvertFrom-Json
+$mainJson = @{"version" = $version; "displayname" = $displayname; "heartbeat" = $heartbeat;}
+$ht2 =@{}
+$dataHash.psobject.properties | Foreach { $ht2[$_.Name] = $_.Value }
+foreach( $dataelem in $ht2.Keys)
+{
+    $mainJson.Add($dataelem,$ht2[$dataelem])
+}
+return $mainJson | ConvertTo-Json
+
