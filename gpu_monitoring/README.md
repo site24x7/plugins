cd8d0941a7f88044c8b0b64e107ec3c0d1c4ae89
site24x7 <site24x7@users.noreply.github.com>
2020-02-17 11:08:19 +0530

    Plugin For Monitoring GPU Device

---
+++ gpu_monitoring/README.md
@@ -0,0 +1,28 @@
+# Plugin for Monitoring GPU Device
+
+### PreRequisites
+
+- Plugin Uses "gpustat" python module to get the gpu device performance metrics	
+	
+      Execute the following command in your server to install gpustat - pip install gpustat
+
+- Plugin Uses "gpustat -cp" command to get the individual core utilization
+
+### Plugin installation
+---
+##### Linux 
+
+- Create a directory "gpu_monitoring" under Site24x7 Linux Agent plugin directory - /opt/site24x7/monagent/plugins/gpu_monitoring
+- Download the file "gpu_monitoring.py" and place it under the "gpu_monitoring" directory
+  
+  wget https://raw.githubusercontent.com/site24x7/plugins/master/gpu_monitoring/gpu_monitoring.py
+	
+  The agent will automatically execute the plugin within five minutes and send performance data to the Site24x7 data center.
+
+### Metrics Captured
+
+- Memory Utilization
+- CPU Utilization
+- Temperature
+- Individual Core Utilization
+- Device Name
---
+++ gpu_monitoring/gpu_monitoring.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import gpustat
+import json
+import subprocess
+
+PLUGIN_VERSION = "1"
+
+HEARTBEAT_REQUIRED = "true"
+
+
+output = gpustat.new_query()
+result_json = {}
+for each_line in output:
+    result_json["memory"] = str(int(each_line.memory_used))
+    result_json["utilization"] = str(int(each_line.utilization))
+    result_json["temperature"] = str(int(each_line.temperature))
+    result_json["device_name"] = each_line.name.split(" ")
+
+result_json["plugin_version"] = PLUGIN_VERSION
+
+result_json["heartbeat_required"] = HEARTBEAT_REQUIRED
+
+
+cmd = 'gpustat -cp'
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+(output, err) = p.communicate()
+p_status = p.wait()
+core_output = output.split('\n')
+for each in core_output:
+    if each.startswith('['):
+        core_out_line = each.split()    
+        core_out="core_"+core_out_line[0].replace('[', '').replace(']', '')
+        result_json[core_out] = core_out_line[5]
+
+print(json.dumps(result_json))
