19d51b6688a9a9c1595f352e0b93b4d50ab12af8
site24x7 <site24x7@users.noreply.github.com>
2020-05-28 16:47:14 +0530

    git hub monitoring
    plugin for monitoring the repositories of a github account added.

---
+++ git_hub/README.md
@@ -0,0 +1,65 @@
+# Plugin for monitoring GitHub
+
+## Plugin installation
+___
+### Prerequisites
+* To monitor your github account we need an username and a Personal Access Token.
+	
+	Please refer this link to create "Personal Access Token" with read access- https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
+
+### Steps (Linux & OSX)
+
+* Create a directory "git_hub" under Site24x7 Linux Agent plugin directory - /opt/site24x7/monagent/plugins/git_hub
+
+* Download the files "git_hub.py" , "git_hub.cfg" and place it under the "git_hub" directory
+
+	wget https://raw.githubusercontent.com/site24x7/plugins/master/git_hub/git_hub.py
+
+	wget https://raw.githubusercontent.com/site24x7/plugins/master/git_hub/git_hub.cfg
+
+### Plugin configuration
+---
+* Open git_hub.py file. Set the values for **USER_NAME**, **PERSONAL_ACCESS_TOKEN**.
+
+* Open the "git_hub.cfg" file and add repository name(s) to be monitored under keyword "repo_name" as given below.
+	Sample configuration file looks like:
+	
+	#git_hub.cfg
+	
+		[repo_1]
+		repo_name="Sample_repo1"
+
+	where repo_1 denotes - plugin monitor name to be displayed in site24x7 client
+
+	repo_name denotes - actual repository name to be monitored
+
+* After completing above steps please run "python git_hub.py --repo_name='Sample_repo1'" to make sure data is obtained.
+
+* If you have more than one repositories needs to be monitored create one more section and configure it as above.
+
+
+### Metrics captured
+---
+* notifications
+* deployments
+* milestones
+* releases
+* pullrequests
+* downloads
+* issues
+* merges
+* issues_comments
+* comments
+* commits
+* subscription
+* contributors
+* subscribers
+* assignees
+* events
+* issue_events
+* teams
+* collaborators
+* is_private_repository
+* default_branch
+* created_at
+* repository_name
---
+++ git_hub/git_hub.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+import requests
+import json
+import argparse
+
+#Enter the User Name of your gitHub Account
+USER_NAME = 'user_name' 
+
+#Enter the PERSONAL_ACCESS_TOKEN of your gitHub Account
+PERSONAL_ACCESS_TOKEN = '6c51c90b6067jhbnmlgfdfghn'
+
+REPO_NAME=""
+GITHUB_URL ="https://api.github.com"
+
+#if any impacting changes to this plugin kindly increment the plugin version here.
+PLUGIN_VERSION = "1"
+
+#Setting this to true will alert you when there is a communication problem while posting plugin data to server
+HEARTBEAT="true"
+
+result_json={}   
+
+class gitHub:
+    count_request_map={}
+    keys_list=[]
+
+    def read_args(self):
+        global REPO_NAME
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--repo_name')
+        args = parser.parse_args()
+        if args.repo_name:
+            REPO_NAME = args.repo_name
+    
+    def construct_urls(self):
+        self.count_request_map['notifications']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/notifications"
+        self.count_request_map['deployments']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/deployments"
+        self.count_request_map['releases']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/releases"
+        self.count_request_map['milestones']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/milestones"
+        self.count_request_map['pull_requests']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/pulls"
+        self.count_request_map['downloads']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/downloads"
+        self.count_request_map['issues']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/issues"
+        self.count_request_map['merges']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/merges"
+        self.count_request_map['issues_comments']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/issues/comments"
+        self.count_request_map['comments']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/comments"
+        self.count_request_map['commits']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/commits"
+        self.count_request_map['subscription']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/subscription"
+        self.count_request_map['contributors']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/contributors"
+        self.count_request_map['subscribers']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/subscribers"
+        self.count_request_map['assignees']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/assignees"
+        self.count_request_map['events']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/events"
+        self.count_request_map['issue_events']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/issues/events"
+        self.count_request_map['teams']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+USER_NAME+"/teams"
+        self.count_request_map['collaborators']=GITHUB_URL+"/repos"+"/"+USER_NAME+"/"+REPO_NAME+"/collaborators"
+
+    def get_metrics_counts(self):
+        global result_json
+        try:
+            for key in self.count_request_map.keys():
+                if key not in self.keys_list:
+                    self.keys_list.append(key)
+                    count_response = requests.get(self.count_request_map.get(key), auth=(USER_NAME,PERSONAL_ACCESS_TOKEN),timeout=3.0)
+                    if count_response.status_code==200:
+                        result_json[key]=len(count_response.json())
+                    else:
+                        result_json[key]=int(0)
+   
+        except Exception as e:
+            result_json['status'] = 0
+            result_json['msg'] = 'Exception occured in collecting data : ' + str(e)
+      
+            
+    def get_repo_details(self):
+        global result_json
+        global REPO_NAME
+        self.repo_check=False
+        try:
+            repo_response = requests.get(GITHUB_URL + '/user/repos', auth=(USER_NAME, PERSONAL_ACCESS_TOKEN),timeout=3.0)       
+            if repo_response.status_code==200:
+                repos_json = repo_response.json()
+                for data in repos_json:  
+                    if data['name'].__eq__(REPO_NAME):
+                        self.construct_urls()
+                        self.get_metrics_counts()
+                        self.repo_check=True
+                        result_json['is_private_repository'] =str(data['private'])
+                        result_json['created_at'] =str(data['created_at'])
+                        result_json['default_branch'] =str(data['default_branch'])
+                        result_json["repository_name"] =REPO_NAME
+                        break;
+            else:
+                result_json['status'] = 0
+                result_json['msg'] = str(repo_response.json()['message'])
+            if self.repo_check is False:
+                result_json['status'] = 0
+                result_json['msg'] = "Given Repository name is not matched - "+REPO_NAME
+        except Exception as e:
+            result_json['status'] = 0
+            result_json['msg'] = 'Exception occured in collecting data : ' + str(e)
+
+    def trigger_action(self):
+        self.read_args()
+        if(not(REPO_NAME and REPO_NAME.strip())): 
+            result_json['status'] = 0
+            result_json['msg'] = 'Repository name is empty'
+            return
+        self.get_repo_details()
+ 
+if __name__ == '__main__':
+    
+    git_hub = gitHub()
+    git_hub.trigger_action()
+    
+    result_json["plugin_version"] = PLUGIN_VERSION
+    result_json["heartbeat_required"] = HEARTBEAT
+
+    print(json.dumps(result_json, indent=4, sort_keys=True))
