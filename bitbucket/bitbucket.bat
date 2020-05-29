5b93bdf2300bced75c6a1729d92557202ca9afc4
site24x7 <site24x7@users.noreply.github.com>
2020-05-29 11:48:23 +0530

    Update bitbucket.bat

--- bitbucket/bitbucket.bat
+++ bitbucket/bitbucket.bat
@@ -1,7 +1,7 @@
 @echo off
 cd "C:\Program Files (x86)\Site24x7\WinAgent\monitoring\Plugins\bitbucket\"
 set HOST=127.0.0.1
-set PORT=4501
+set PORT=3333
 set PLUGIN_VERSION=1
 set HEARTBEAT_REQUIRED=true
 set RMI_UNAME=
