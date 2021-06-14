9902377d01defe84edb0ba510284be81ed034128
Kavin Kumar <40456399+mrkksparrow@users.noreply.github.com>
2021-06-14 20:33:26 +0530

    Update proxysql.py

--- proxysql/proxysql.py
+++ proxysql/proxysql.py
@@ -27,7 +27,6 @@ result_json = {}
 
 
 METRIC_UNITS = {
-    "proxysql_uptime" : "seconds",
     "sqlite3_memory_bytes" : "bytes",
     "active_transactions" : "transactions",
     "client_connections_aborted" : "connections",
@@ -40,7 +39,6 @@ METRIC_UNITS = {
     "backend_query_time_nsec" : "nanoseconds",
     "mysql_backend_buffers_bytes" : "bytes",
     "mysql_frontend_buffers_bytes" : "bytes",
-    "mysql_session_internal_bytes" : "bytes",
     "mysql_thread_workers" : "workers",
     "mysql_monitor_workers" : "workers",
     "connpool_get_conn_success" : "connections",
@@ -53,79 +51,35 @@ METRIC_UNITS = {
     "stmt_server_active_total" : "units",
     "stmt_server_active_unique" : "units",
     "stmt_cached" : "units",
-    "query_cache_memory_bytes" : "bytes",
-    "query_cache_entries" : "entries",
-    "query_cache_purged" : "entries",
-    "query_cache_bytes_in" : "bytes",
-    "query_cache_bytes_out" : "bytes",
-    "query_cache_count_get" : "queries",
-    "query_cache_count_get_ok" : "queries",
-    "query_cache_count_set" : "queries",
     "query_processor_time_nsec" : "nanoseconds",
-    "jemalloc_resident" : "bytes",
-    "jemalloc_active" : "bytes",
-    "jemalloc_mapped" : "bytes",
-    "jemalloc_allocated" : "bytes",
-    "jemalloc_metadata" : "bytes",
-    "jemalloc_retained" : "bytes",
-    "Auth_memory" : "bytes",
-    "query_digest_memory" : "bytes",
-    "stack_memory_mysql_threads" : "bytes",
-    "stack_memory_admin_threads" : "bytes",
-    "stack_memory_cluster_threads" : "bytes",
 }
 
 proxysql_metrics = {
-    "mysql_global_metrics" : {
-        "ProxySQL_Uptime" : "proxysql_uptime",
-        "SQLite3_memory_bytes" : "sqlite3_memory_bytes",
-        "Active_Transactions" : "active_transactions",
-        "Client_Connections_aborted" : "client_connections_aborted",
-        "Client_Connections_connected" : "client_connections_connected",
-        "Client_Connections_created" : "client_connections_created",
-        "Server_Connections_aborted" : "server_connections_aborted",
-        "Server_Connections_connected" : "server_connections_connected",
-        "Server_Connections_created" : "server_connections_created",
-        "Client_Connections_non_idle" : "client_connections_non_idle",
-        "Backend_query_time_nsec" : "backend_query_time_nsec",
-        "mysql_backend_buffers_bytes" : "mysql_backend_buffers_bytes",
-        "mysql_frontend_buffers_bytes" : "mysql_frontend_buffers_bytes",
-        "mysql_session_internal_bytes" : "mysql_session_internal_bytes",
-        "MySQL_Thread_Workers" : "mysql_thread_workers",
-        "MySQL_Monitor_Workers" : "mysql_monitor_workers",
-        "ConnPool_get_conn_success" : "connpool_get_conn_success",
-        "ConnPool_get_conn_immediate" : "connpool_get_conn_immediate",
-        "Questions" : "questions",
-        "Slow_queries" : "slow_queries",
-        "ConnPool_memory_bytes" : "connpool_memory_bytes",
-        "Stmt_Client_Active_Total" : "stmt_client_active_total",
-        "Stmt_Client_Active_Unique" : "stmt_client_active_unique",
-        "Stmt_Server_Active_Total" : "stmt_server_active_total",
-        "Stmt_Server_Active_Unique" : "stmt_server_active_unique",
-        "Stmt_Cached" : "stmt_cached",
-        "Query_Cache_Memory_bytes" : "query_cache_memory_bytes",
-        "Query_Cache_Entries" : "query_cache_entries",
-        "Query_Cache_Purged" : "query_cache_purged",
-        "Query_Cache_bytes_IN" : "query_cache_bytes_in",
-        "Query_Cache_bytes_OUT" : "query_cache_bytes_out",
-        "Query_Cache_count_GET" : "query_cache_count_get",
-        "Query_Cache_count_GET_OK" : "query_cache_count_get_ok",
-        "Query_Cache_count_SET" : "query_cache_count_set",
-        "Query_Processor_time_nsec" : "query_processor_time_nsec",
-    },
-    "stats_memory_metrics" : {
-        "jemalloc_resident" : "jemalloc_resident",
-        "jemalloc_active" : "jemalloc_active",
-        "jemalloc_mapped" : "jemalloc_mapped",
-        "jemalloc_allocated" : "jemalloc_allocated",
-        "jemalloc_metadata" : "jemalloc_metadata",
-        "jemalloc_retained" : "jemalloc_retained",
-        "Auth_memory" : "Auth_memory",
-        "query_digest_memory" : "query_digest_memory",
-        "stack_memory_mysql_threads" : "stack_memory_mysql_threads",
-        "stack_memory_admin_threads" : "stack_memory_admin_threads",
-        "stack_memory_cluster_threads" : "stack_memory_cluster_threads",
-    }
+    "SQLite3_memory_bytes" : "sqlite3_memory_bytes",
+    "Active_Transactions" : "active_transactions",
+    "Client_Connections_aborted" : "client_connections_aborted",
+    "Client_Connections_connected" : "client_connections_connected",
+    "Client_Connections_created" : "client_connections_created",
+    "Server_Connections_aborted" : "server_connections_aborted",
+    "Server_Connections_connected" : "server_connections_connected",
+    "Server_Connections_created" : "server_connections_created",
+    "Client_Connections_non_idle" : "client_connections_non_idle",
+    "Backend_query_time_nsec" : "backend_query_time_nsec",
+    "mysql_backend_buffers_bytes" : "mysql_backend_buffers_bytes",
+    "mysql_frontend_buffers_bytes" : "mysql_frontend_buffers_bytes",
+    "MySQL_Thread_Workers" : "mysql_thread_workers",
+    "MySQL_Monitor_Workers" : "mysql_monitor_workers",
+    "ConnPool_get_conn_success" : "connpool_get_conn_success",
+    "ConnPool_get_conn_immediate" : "connpool_get_conn_immediate",
+    "Questions" : "questions",
+    "Slow_queries" : "slow_queries",
+    "ConnPool_memory_bytes" : "connpool_memory_bytes",
+    "Stmt_Client_Active_Total" : "stmt_client_active_total",
+    "Stmt_Client_Active_Unique" : "stmt_client_active_unique",
+    "Stmt_Server_Active_Total" : "stmt_server_active_total",
+    "Stmt_Server_Active_Unique" : "stmt_server_active_unique",
+    "Stmt_Cached" : "stmt_cached",
+    "Query_Processor_time_nsec" : "query_processor_time_nsec",
 }
 
 
@@ -162,10 +116,7 @@ def get_output():
         mycursor = mydb.cursor()
         
         QUERY = "select * from stats_mysql_global"
-        result = fetch_metrics_via_mysqlconnector(mycursor, QUERY, proxysql_metrics["mysql_global_metrics"])
-        
-        QUERY = "select * from stats_memory_metrics"
-        result.update(fetch_metrics_via_mysqlconnector(mycursor, QUERY, proxysql_metrics["stats_memory_metrics"]))
+        result = fetch_metrics_via_mysqlconnector(mycursor, QUERY, proxysql_metrics)
         
     except Exception as e:
         result["status"] = 0
