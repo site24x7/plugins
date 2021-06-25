a56b935ac1e69d04400224d13a827c7929e6c5c5
Sathvika-a <86009124+Sathvika-a@users.noreply.github.com>
2021-06-25 19:23:58 +0530

    Update mongodb_measurement_of_process.py

--- mongodb_measurement_of_process/mongodb_measurement_of_process.py
+++ mongodb_measurement_of_process/mongodb_measurement_of_process.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 import sys
-import time
 import json
 import requests
-import subprocess
-import os
 import argparse
 from requests.auth import HTTPDigestAuth
 
@@ -25,17 +22,24 @@ heartbeat_required = "true"
 resultjson={}
 
 metric_units={
-    "NETWORK_BYTES_IN":"byte",
-    "NETWORK_BYTES_OUT":"byte",
-    "LOGICAL_SIZE":"byte"
+    "network.in":"bytes/second",
+    "network.out":"bytes/second",
+    "network.request":"count/second",
+    "opcounter.cmd":"count/second",
+    "opcounter.query":"count/second",
+    "opcounter.update":"count/second",
+    "opcounter.delete":"count/second",
+    "opcounter.getmore":"count/second",
+    "opcounter.insert":"count/second",
+    "logicalsize":"bytes"
 }
 
 
-group_id="6080fc32b449622900b6612e"
-host="cluster0-shard-00-02.bcgaj.mongodb.net"
-port="27017"
-public_key = 'jifqsfrc'
-private_key = '80d11231-0482-4387-8107-ea0336380452'
+group_id= ""
+host= ""
+port= ""
+public_key = ""
+private_key = ""
 
 
 def metrics_collector():
@@ -44,27 +48,26 @@ def metrics_collector():
         url = "https://cloud.mongodb.com/api/atlas/v1.0/groups/"+group_id+"/processes/"+host+":"+port+"/measurements?granularity=PT5M&period=PT5M&pretty=true" 
         data=json.loads((requests.get(url, auth=HTTPDigestAuth(public_key, private_key)).content))
         new_data = {}
-        new_data["groupId"]=data["groupId"]
-        new_data["hostId"]=data["hostId"]
+        new_data["groupid"]=data["groupId"]
+        new_data["hostid"]=data["hostId"]
         new_data["start"]=data["start"]
         new_data["end"]=data["end"]
-        new_data["CONNECTIONS"] = data["measurements"][0]["dataPoints"][0]["value"]
-        new_data["NETWORK_BYTES_IN"]=data["measurements"][1]["dataPoints"][0]["value"]
-        new_data["NETWORK_BYTES_OUT"]=data["measurements"][2]["dataPoints"][0]["value"]
-        new_data["NETWORK_NUM_REQUESTS"]=data["measurements"][3]["dataPoints"][0]["value"]
-        new_data["OPCOUNTER_CMD"]=data["measurements"][4]["dataPoints"][0]["value"]
-        new_data["OPCOUNTER_QUERY"]=data["measurements"][5]["dataPoints"][0]["value"]
-        new_data["OPCOUNTER_UPDATE"]=data["measurements"][6]["dataPoints"][0]["value"]
-        new_data["OPCOUNTER_DELETE"]=data["measurements"][7]["dataPoints"][0]["value"]
-        new_data["OPCOUNTER_GETMORE"]=data["measurements"][8]["dataPoints"][0]["value"]
-        new_data["OPCOUNTER_INSERT"]=data["measurements"][9]["dataPoints"][0]["value"]
-        new_data["LOGICAL_SIZE"]=data["measurements"][10]["dataPoints"][0]["value"]
+        new_data["connections"] = data["measurements"][0]["dataPoints"][0]["value"]
+        new_data["network.in"]=data["measurements"][1]["dataPoints"][0]["value"]
+        new_data["network.out"]=data["measurements"][2]["dataPoints"][0]["value"]
+        new_data["network.request"]=data["measurements"][3]["dataPoints"][0]["value"]
+        new_data["opcounter.cmd"]=data["measurements"][4]["dataPoints"][0]["value"]
+        new_data["opcounter.query"]=data["measurements"][5]["dataPoints"][0]["value"]
+        new_data["opcounter.update"]=data["measurements"][6]["dataPoints"][0]["value"]
+        new_data["opcounter.delete"]=data["measurements"][7]["dataPoints"][0]["value"]
+        new_data["opcounter.getmore"]=data["measurements"][8]["dataPoints"][0]["value"]
+        new_data["opcounter.insert"]=data["measurements"][9]["dataPoints"][0]["value"]
+        new_data["logicalsize"]=data["measurements"][10]["dataPoints"][0]["value"]
         
 
         
         return new_data
 
-        
         
     
         
