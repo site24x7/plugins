50393a492b31d8ab5fd0144d9c01b90737948571
ani3588 <ani3588@gmail.com>
2021-07-07 11:19:01 +0530

    Supporting more metrics and multicfg for apache

--- apache/apache.py
+++ apache/apache.py
@@ -3,106 +3,138 @@
 import sys
 import json
 
-# Change the Apache stats URL accordingly here. Retain the "?auto" suffix.
-url = "http://localhost:80/server-status?auto"
-username = None
-password = None
+'''
+Created on 21-Jun-2021
 
-# if any impacting changes to this plugin kindly increment the plugin version here
-PLUGIN_VERSION = "1"
-
-# Setting this to true will alert you when there is a communication problem while posting plugin data to server
-HEARTBEAT = "true"
-
-dict_reqdMet = {'Total Accesses':'total_accesses',
-                'Total kBytes':'total_kbytes',
-                'CPULoad':'cpu_load',
-                'Uptime':'uptime',
-                'ReqPerSec':'req_per_sec',
-                'BytesPerSec':'bytes_per_sec',
-                'BytesPerReq':'bytes_per_req',
-                'BusyWorkers':'busy_workers',
-                'IdleWorkers':'idle_workers'
-                }
-
-METRICS_UNITS = {'total_kbytes':'Bytes',
-                 'uptime':'Seconds',
-                 'bytes_per_sec':'Bytes',
-                 'bytes_per_req':'Bytes'
-                 }
+@author: anita-1372
+'''
 
 PYTHON_MAJOR_VERSION = sys.version_info[0]
+
 if PYTHON_MAJOR_VERSION == 3:
     import urllib
     import urllib.request as urlconnection
     from urllib.error import URLError, HTTPError
-    from http.client import InvalidURL
+    from urllib.request import ProxyHandler
 elif PYTHON_MAJOR_VERSION == 2:
     import urllib2 as urlconnection
     from urllib2 import HTTPError, URLError
     from httplib import InvalidURL
 
-class apache():
-    def __init__(self):
-        self._userName = username
-        self._userPass = password
-        self._url = url
-        self.dictApacheData = {}
 
-    def main(self):
-        self.metric_collector()
-        print(str(json.dumps(self.dictApacheData)))
+
+METRICS = { 'Total Accesses':'total_accesses',
+            'Total kBytes':'total_kbytes',
+            'CPULoad':'cpu_load',
+            'Uptime':'uptime',
+            'ReqPerSec':'req_per_sec',
+            'BytesPerSec':'bytes_per_sec',
+            'BytesPerReq':'bytes_per_req',
+            'BusyWorkers':'busy_workers',
+            'IdleWorkers':'idle_workers',
+            'ServerVersion' : 'version',
+            'Load1' : 'load1',
+            'Load5' : 'load5',
+            'Load15' : 'load15',
+            'CPUUser' : 'cpu_user',
+            'CPUSystem' : 'cpu_system',
+            'ConnsTotal' : 'total_connections',
+            'ConnsAsyncWriting' : 'connections_async_writing',
+            'ConnsAsyncKeepAlive' : 'connections_async_keep_alive',
+            'ConnsAsyncClosing' : 'connections_async_closing'
+        }
+
+UNITS = {   'total_kbytes':'Bytes',
+            'uptime':'Seconds',
+            'bytes_per_sec':'Bytes',
+            'bytes_per_req':'Bytes'
+        }
+
+class ApacheMonitoring(object):
+    '''
+    Class to monitor Apache webserver
+    '''
+    def __init__(self, args):
+        '''
+        Constructor
+        '''
+        self.url=args.url
+        self.username=args.username
+        self.password=args.password  
+        
+        self.timeout = args.timeout
+        
+        self.plugin_version=args.plugin_version
+        self.heartbeat=args.heartbeat  
+        self.data = {}
+        
+        self._collect_metrics()
+        
+        self.data['plugin_version'] = self.plugin_version
+        self.data['heartbeat_required'] = self.heartbeat
+        self.data['units'] = UNITS
+        
     
-    def metric_collector(self):
+    def _parse_data_(self, _data_):
         try:
-            if self._userName and self._userPass:
-                password_mgr = urlconnection.HTTPPasswordMgrWithDefaultRealm()
-                password_mgr.add_password(None, url, self._userName, self._userPass)
-                auth_handler = urlconnection.HTTPBasicAuthHandler(password_mgr)
-                opener = urlconnection.build_opener(auth_handler)
-                urlconnection.install_opener(opener)
-            response = urlconnection.urlopen(url, timeout=10)
-            if response.getcode() == 200:
-                byte_responseData = response.read()
-                str_responseData = byte_responseData.decode('UTF-8')
-                self._parseStats(str_responseData)
-            else:
-                self.dictApacheData['status'] = 0
-                self.dictApacheData['msg'] = 'Error_code' + str(response.getcode())
-        except HTTPError as e:
-            self.dictApacheData['status'] = 0
-            self.dictApacheData['msg'] = 'Error_code : HTTP Error ' + str(e.code)
-        except URLError as e:
-            self.dictApacheData['status'] = 0
-            self.dictApacheData['msg'] = 'Error_code : URL Error ' + str(e.reason)
-        except InvalidURL as e:
-            self.dictApacheData['status'] = 0
-            self.dictApacheData['msg'] = 'Error_code : Invalid URL'
-        except Exception as e:
-            self.dictApacheData['status'] = 0
-            self.dictApacheData['msg'] = 'Exception occured in collecting data : ' + str(e)
-    
-    def _parseStats(self, str_responseData):
-        try:
-            # dictApacheData = {}
-            listStatsData = str_responseData.split('\n')
+            listStatsData = _data_.split('\n')
             for eachStat in listStatsData:
                 stats = eachStat.split(':')
-                if str(stats[0]) in dict_reqdMet:
-                    self.dictApacheData.setdefault(dict_reqdMet[str(stats[0])], str.strip(str(stats[1])))
-            self.dictApacheData['plugin_version'] = PLUGIN_VERSION
-            self.dictApacheData['heartbeat_required'] = HEARTBEAT
-            self.dictApacheData['units'] = METRICS_UNITS
-            # print(str(json.dumps(dictApacheData)))
+                if str(stats[0]) in METRICS: self.data.setdefault(METRICS[str(stats[0])], str.strip(str(stats[1])))
+            
         except TypeError as e:
-            self.dictApacheData['status'] = 0
-            self.dictApacheData['msg'] = 'Type error in _parseStats'
-            # print(str(json.dumps({'Error_code': 'Type error in _parseStats'})))
+            self.data['status'] = 0
+            self.data['msg'] = 'Error in parsing'
+            
         except Exception as e:
-            self.dictApacheData['status'] = 0
-            self.dictApacheData['msg'] = 'Exception in _parse stats' + str(e)
-            # print(str(json.dumps({'Error_code':str(e)})))
-
+            self.data['status'] = 0
+            self.data['msg'] = 'Exception in parse stats' + str(e)
+            
+    
+    def _collect_metrics(self):
+        try:
+            auth_handler = None
+                            
+            if self.username and self.password:
+                password_mgr = urlconnection.HTTPPasswordMgrWithDefaultRealm()
+                password_mgr.add_password(None, self.url, self.username, self.password)
+                auth_handler = urlconnection.HTTPBasicAuthHandler(password_mgr)  
+            
+            if auth_handler is not None :
+                opener = urlconnection.build_opener(auth_handler)
+                urlconnection.install_opener(opener)
+            
+            response = urlconnection.urlopen(self.url, timeout=self.timeout)
+            
+            if response.getcode() == 200:
+                response_data = response.read().decode('UTF-8')
+                self._parse_data_(response_data)
+            else:
+                self.data['status'] = 0
+                self.data['msg'] = 'Error_code' + str(response.getcode())
+                
+        except Exception as e:
+            self.data['status'] = 0
+            self.data['msg'] = str(e) + ": " + self.url
+        return self.data
+    
+        
 if __name__ == '__main__':
-    ap = apache()
-    ap.main()
+    
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', help='apache monitoring url', nargs='?', default="http://localhost:80/server-status?auto")
+    parser.add_argument('--username',  help='user name', nargs='?', default= None)
+    parser.add_argument('--password', help='password', nargs='?', default= None)
+    parser.add_argument('--timeout', help='timeout', nargs='?', type=int, default=30)
+    
+    parser.add_argument('--plugin_version', help='plugin template version', type=int,  nargs='?', default=1)
+    parser.add_argument('--heartbeat', help='alert if monitor does not send data', type=bool, nargs='?', default=True)
+    
+    
+    args = parser.parse_args()
+    
+    apache = ApacheMonitoring(args)
+    data = apache._collect_metrics()
+    print(json.dumps(data, indent=4, sort_keys=True))
+    
