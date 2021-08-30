8a95768a133e6ae499d9652f71575b274b821d8e
Sathvika-a <86009124+Sathvika-a@users.noreply.github.com>
2021-08-30 11:12:28 +0530

    Update filebeat.py

--- filebeat/filebeat.py
+++ filebeat/filebeat.py
@@ -5,19 +5,19 @@ import urllib
 import urllib.request as connector
 
 metric_units={
-    "total.freespace":"bytes",
-    "total.spaceused":"bytes",
-    "total.space":"bytes",
-    "tcp.established":"bytes",
-    "tcp.listening":"bytes",
-    "tcp.closing":"bytes",
-    "network.in":"bytes",
-    "network.out":"bytes",
-    "network.in.errors":"bytes",
-    "network.out.dropped":"bytes",
-    "network.out.errors":"bytes",
-    "syn.sent":"bytes",
-    "syn.recv":"bytes"
+    "total_freespace":"bytes",
+    "total_spaceused":"bytes",
+    "total_space":"bytes",
+    "tcp_established":"bytes",
+    "tcp_listening":"bytes",
+    "tcp_closing":"bytes",
+    "network_in":"bytes",
+    "network_out":"bytes",
+    "network_in_errors":"bytes",
+    "network_out_dropped":"bytes",
+    "network_out_errors":"bytes",
+    "syn_sent":"bytes",
+    "syn_recv":"bytes"
 }
 
 
@@ -40,40 +40,40 @@ class Filebeat(object):
 
         try:
             url="http://"+self.host+":"+self.port+"/metricbeat-*/_search?pretty"
-            data1=((connector.urlopen(url)).read()).decode("UTF-8")
-            data1=json.loads(data1)
-            data=data1["hits"]["hits"][1]["_source"]
-            self.resultjson["system.type"]=data["service"]["type"]
-            self.resultjson["network1.name"]=data["system"]["network"]["name"]
+            response=((connector.urlopen(url)).read()).decode("UTF-8")
+            response=json.loads(response)
+            data=response["hits"]["hits"][1]["_source"]
+            self.resultjson["system_type"]=data["service"]["type"]
+            self.resultjson["network1_name"]=data["system"]["network"]["name"]
             name=data["system"]["network"]["name"]
-            self.resultjson[name+".out"]=data["system"]["network"]["out"]["bytes"]
-            self.resultjson[name+".out.errors"]=data["system"]["network"]["out"]["errors"]
-            self.resultjson[name+".out.dropped"]=data["system"]["network"]["out"]["dropped"]
-            self.resultjson[name+".in"]=data["system"]["network"]["in"]["bytes"]
-            self.resultjson[name+".in.errors"]=data["system"]["network"]["in"]["bytes"]
-            self.resultjson[name+".out.dropped"]=data["system"]["network"]["in"]["dropped"]
-            data=data1["hits"]["hits"][3]["_source"]
-            self.resultjson["network2.name"]=data["system"]["network"]["name"]
+            self.resultjson[name+"_out"]=data["system"]["network"]["out"]["bytes"]
+            self.resultjson[name+"_out_errors"]=data["system"]["network"]["out"]["errors"]
+            self.resultjson[name+"_out_dropped"]=data["system"]["network"]["out"]["dropped"]
+            self.resultjson[name+"_in"]=data["system"]["network"]["in"]["bytes"]
+            self.resultjson[name+"_in_errors"]=data["system"]["network"]["in"]["bytes"]
+            self.resultjson[name+"_out_dropped"]=data["system"]["network"]["in"]["dropped"]
+            data=response["hits"]["hits"][3]["_source"]
+            self.resultjson["network2_name"]=data["system"]["network"]["name"]
             name=data["system"]["network"]["name"]
-            self.resultjson[name+".out"]=data["system"]["network"]["out"]["bytes"]
-            self.resultjson[name+".out.errors"]=data["system"]["network"]["out"]["errors"]
-            self.resultjson[name+".out.dropped"]=data["system"]["network"]["out"]["dropped"]
-            self.resultjson[name+".in"]=data["system"]["network"]["in"]["bytes"]
-            self.resultjson[name+".in.errors"]=data["system"]["network"]["in"]["bytes"]
-            self.resultjson[name+".out.dropped"]=data["system"]["network"]["in"]["dropped"]       
-            data=data1["hits"]["hits"][5]["_source"]["system"]["fsstat"]
+            self.resultjson[name+"_out"]=data["system"]["network"]["out"]["bytes"]
+            self.resultjson[name+"_out_errors"]=data["system"]["network"]["out"]["errors"]
+            self.resultjson[name+"_out_dropped"]=data["system"]["network"]["out"]["dropped"]
+            self.resultjson[name+"_in"]=data["system"]["network"]["in"]["bytes"]
+            self.resultjson[name+"_in_errors"]=data["system"]["network"]["in"]["bytes"]
+            self.resultjson[name+"_out_dropped"]=data["system"]["network"]["in"]["dropped"]       
+            data=response["hits"]["hits"][5]["_source"]["system"]["fsstat"]
         
-            self.resultjson["total.freespace"]=data["total_size"]["free"]
-            self.resultjson["total.spaceused"]=data["total_size"]["used"]
-            self.resultjson["total.space"]=data["total_size"]["total"]
-            self.resultjson["total.files"]=data["total_files"]
-            data=data1["hits"]["hits"][7]["_source"]["system"]["socket"]["summary"]["tcp"]["all"]
-            self.resultjson["tcp.listening"]=data["listening"]
-            self.resultjson["tcp.established"]=data["established"]
-            self.resultjson["tcp.closing"]=data["closing"]
-            self.resultjson["tcp.count"]=data["count"]
-            self.resultjson["syn.recv"]=data["syn_recv"]
-            self.resultjson["syn.sent"]=data["syn_sent"]
+            self.resultjson["total_freespace"]=data["total_size"]["free"]
+            self.resultjson["total_spaceused"]=data["total_size"]["used"]
+            self.resultjson["total_space"]=data["total_size"]["total"]
+            self.resultjson["total_files"]=data["total_files"]
+            data=response["hits"]["hits"][7]["_source"]["system"]["socket"]["summary"]["tcp"]["all"]
+            self.resultjson["tcp_listening"]=data["listening"]
+            self.resultjson["tcp_established"]=data["established"]
+            self.resultjson["tcp_closing"]=data["closing"]
+            self.resultjson["tcp_count"]=data["count"]
+            self.resultjson["syn_recv"]=data["syn_recv"]
+            self.resultjson["syn_sent"]=data["syn_sent"]
        
         except Exception as e:
             self.resultjson["msg"]=str(e)
