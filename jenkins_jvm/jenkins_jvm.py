41690b20642859262eceb409a439c21ffdfd2df4
Sathvika-a <86009124+Sathvika-a@users.noreply.github.com>
2021-08-30 17:45:25 +0530

    Update jenkins_jvm.py

--- jenkins_jvm/jenkins_jvm.py
+++ jenkins_jvm/jenkins_jvm.py
@@ -9,18 +9,18 @@ from urllib.request import ProxyHandler
 
 
 metric_units={
-    "Heap Memory Commited":"bytes",
-    "Heap Memory Initiated":"bytes",
-    "Maximum Heap Memory":"bytes",
-    "Heap Memory Used":"bytes",
-    "Non-Heap Memory Commited":"bytes",
-    "Non-Heap Memory Initiated":"bytes",
-    "Maximum Non-Heap Memory":"bytes",
-    "Non-Heap Memory Used":"bytes",
-    "Total Memory Commited":"bytes",
-    "Total Memory Initiated":"bytes",
-    "Total Maximum Memory":"bytes",
-    "Total Memory Used":"bytes"
+    "heap_memory_commited":"bytes",
+    "heap_memory_initiated":"bytes",
+    "maximum_heap_memory":"bytes",
+    "heap_memory_used":"bytes",
+    "non-heap_memory_commited":"bytes",
+    "non-heap_memory_initiated":"bytes",
+    "maximum_non-heap_memory":"bytes",
+    "non-heap_memory_used":"bytes",
+    "total_memory_commited":"bytes",
+    "total_memory_initiated":"bytes",
+    "total_maximum_memory":"bytes",
+    "total_memory_used":"bytes"
 }
 
 class Jenkins(object):
@@ -44,30 +44,30 @@ class Jenkins(object):
         try:
             url="http://"+self.host+":"+self.port+"/metrics/"+self.apikey+"/metrics?pretty=true"
             auth_handler = urlconnection.HTTPBasicAuthHandler((urlconnection.HTTPPasswordMgrWithDefaultRealm()). add_password(None, url, self.username, self.password) )
-            data = (urlconnection.urlopen(url)).read().decode('UTF-8')
-            data=json.loads(data)
-            data1=data["gauges"]
-            self.resultjson["Blocked Thread"]=data1["vm.blocked.count"]["value"]
-            self.resultjson["Thread Count"]=data1["vm.count"]["value"]
-            self.resultjson["Deadlock Count"]=data1["vm.deadlock.count"]["value"]
-            self.resultjson["File descriptor Ratio"]=data1["vm.file.descriptor.ratio"]["value"]
-            self.resultjson["Heap Memory Commited"]=data1["vm.memory.heap.committed"]["value"]
-            self.resultjson["Heap Memory Initiated"]=data1["vm.memory.heap.init"]["value"]
-            self.resultjson["Maximum Heap Memory"]=data1["vm.memory.heap.max"]["value"]
-            self.resultjson["Heap Memory Used"]=data1["vm.memory.heap.used"]["value"]
-            self.resultjson["Non-Heap Memory Commited"]=data1["vm.memory.non-heap.committed"]["value"]
-            self.resultjson["Non-Heap Memory Initiated"]=data1["vm.memory.non-heap.init"]["value"]
-            self.resultjson["Maximum Non-Heap Memory"]=data1["vm.memory.non-heap.max"]["value"]
-            self.resultjson["Non-Heap Memory Used"]=data1["vm.memory.non-heap.used"]["value"]
-            self.resultjson["Total Memory Commited"]=data1["vm.memory.total.committed"]["value"]
-            self.resultjson["Total Memory Initiated"]=data1["vm.memory.total.init"]["value"]
-            self.resultjson["Total Maximum Memory"]=data1["vm.memory.total.max"]["value"]
-            self.resultjson["Total Memory Used"]=data1["vm.memory.total.used"]["value"]
-            self.resultjson["New Threads"]=data1["vm.new.count"]["value"]
-            self.resultjson["Running Threads"]=data1["vm.runnable.count"]["value"]
-            self.resultjson["Terminated Threads"]=data1["vm.terminated.count"]["value"]
-            self.resultjson["Suspended Threads"]=data1["vm.timed_waiting.count"]["value"]
-            self.resultjson["Waiting Threads"]=data1["vm.waiting.count"]["value"]
+            response = (urlconnection.urlopen(url)).read().decode('UTF-8')
+            response=json.loads(response)
+            data=response["gauges"]
+            self.resultjson["blocked_thread"]=data["vm.blocked.count"]["value"]
+            self.resultjson["thread_count"]=data["vm.count"]["value"]
+            self.resultjson["deadlock_count"]=data["vm.deadlock.count"]["value"]
+            self.resultjson["file_descriptor_ratio"]=data["vm.file.descriptor.ratio"]["value"]
+            self.resultjson["heap_memory_commited"]=data["vm.memory.heap.committed"]["value"]
+            self.resultjson["heap_memory_initiated"]=data["vm.memory.heap.init"]["value"]
+            self.resultjson["maximum_heap_memory"]=data["vm.memory.heap.max"]["value"]
+            self.resultjson["heap_memory_used"]=data["vm.memory.heap.used"]["value"]
+            self.resultjson["non-heap_memory_commited"]=data["vm.memory.non-heap.committed"]["value"]
+            self.resultjson["non-heap_memory_initiated"]=data["vm.memory.non-heap.init"]["value"]
+            self.resultjson["maximum_non-heap_memory"]=data["vm.memory.non-heap.max"]["value"]
+            self.resultjson["non-heap_memory_used"]=data["vm.memory.non-heap.used"]["value"]
+            self.resultjson["total_memory_commited"]=data["vm.memory.total.committed"]["value"]
+            self.resultjson["total_memory_initiated"]=data["vm.memory.total.init"]["value"]
+            self.resultjson["total_maximum_memory"]=data["vm.memory.total.max"]["value"]
+            self.resultjson["total_memory_used"]=data["vm.memory.total.used"]["value"]
+            self.resultjson["new_threads"]=data["vm.new.count"]["value"]
+            self.resultjson["running_threads"]=data["vm.runnable.count"]["value"]
+            self.resultjson["terminated_threads"]=data["vm.terminated.count"]["value"]
+            self.resultjson["suspended_threads"]=data["vm.timed_waiting.count"]["value"]
+            self.resultjson["waiting_threads"]=data["vm.waiting.count"]["value"]
             
         except Exception as e:
             self.resultjson["msg"]=str(e)
