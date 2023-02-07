3a4b1af3c7b879c4b1a643ac0450a72628cb50d9
Tharun Raj <122536970+TharunRajTR@users.noreply.github.com>
2023-02-07 11:57:19 +0530

    Update cifs.py

--- cifs/cifs.py
+++ cifs/cifs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import json
 import platform
@@ -29,46 +29,54 @@ MOUNT = ""
 
 result = {}
 
-def get_cifs_mount_data (output, ipaddr):
+def get_cifs_mount_data (output, ipaddr,count,mpath):
     data = {}
+
     try:
+
         output = ' '.join(output.split())
         output = output.split(" ")
         server = output[0].split(ipaddr)
-        data['shared_directory'] = server[1]
-        data['mount_point'] = MOUNT
-        data['disk_usage'] = int(output[4].replace('%', ''))
-            
+        data['Folder '+count+' Shared Directory'] = server[1]
+        data[mpath+' Disk Usage'] = int(output[4].replace('%', ''))
+
     except Exception as e:
         data['status'] = 0
         data['msg'] = str(e)
+        #print(e)
         
     return data
     
     
-def get_cifs_mount_info (output):
+def get_cifs_mount_info (output,count):
     data = {}
     try:
         output = ' '.join(output.split())
         output = output.split(" ")
+
         for out in output:
-            if "vers" in out and "domain" in out:
+            if "vers" in out or "domain" in out:
                 out = out.split(",")
                 for each in out:
+
                     if "vers" in each:
                         each = each.split("=")    
-                        data['cifs_version'] = str(each[1])
+                        data['Cifs Version'] = str(each[1])
+
                     if each=="rw":
-                        data['mount_permission'] = "read/write"
+                        data['Folder '+count+' Mount Permission'] = "read/write"
+
                     if each=="ro":
-                        data['mount_permission'] = "read only"
+                        data['Folder '+count+' Mount Permission'] = "read only"
+
                     if "addr" in each:
                         each = each.split("=")
-                        data['server_ip_address'] = str(each[1])
+                        data['Folder ' +count+' Server IP Address'] = str(each[1])
+
                     if "domain" in each:
                         each = each.split("=")
-                        data['domain'] = str(each[1])
-    
+                        data['Folder '+count+' Domain'] = str(each[1])
+        
     except Exception as e:
         data['status'] = 0
         data['msg'] = str(e)
@@ -76,7 +84,7 @@ def get_cifs_mount_info (output):
     return data
 
 
-def metricCollector():
+def metricCollector(MOUNT,folder):
     data = {}
 
     try:
@@ -86,19 +94,24 @@ def metricCollector():
         top_output = proc.communicate()[0]
         top_output=top_output.strip()
         top_output=top_output.decode("utf-8")
+        data['Folder '+folder+' Mount Point'] = MOUNT
+        
         if "@@@@@@" in top_output:
             output = top_output.split("@@@@@@")
             top_output = output[1]
             output = output[0]
+
         if '%%' in top_output:
-            data.update(get_cifs_mount_info(output))
-            data.update(get_cifs_mount_data(top_output, data['server_ip_address']))
+            data.update(get_cifs_mount_info(output,folder))
+            data.update(get_cifs_mount_data(top_output, data["Folder "+folder+' Server IP Address'],folder,MOUNT))
+            data["Folder "+folder+" Status"]= "Mounted"
+            
         elif '-1' in top_output:
-            data['status']=0
-            data['msg']= MOUNT+" is unmounted"
+            data["Folder "+folder+" Status"]= "Unmounted"
+
         elif '-2' in top_output:
-            data['status']=0
-            data['msg']= MOUNT+" - CIFS Server Host is not reachable"
+            data["Folder "+folder+" Status"]= "Server not reachable"
+
     except Exception as e:
         data['status']=0
         data['msg']=str(e)
@@ -113,9 +126,14 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     if args.mount_folder:
-        MOUNT = args.mount_folder
-    
-    result = metricCollector()
+        folders = args.mount_folder
+        folders=folders.split(",")
+    n=1
+
+    for i in folders:
+        result_collector = metricCollector(i,str(n))
+        n+=1
+        result.update(result_collector )
     
     result['plugin_version'] = PLUGIN_VERSION
     result['heartbeat_required']=HEARTBEAT
