576c9d82eef1af6e1ab3554102b9f92d56f9f5fd
Sathvika-a <86009124+Sathvika-a@users.noreply.github.com>
2023-02-16 13:28:19 +0530

    mssql custom query update

--- mssql_custom_query/mssql_custom_query.ps1
+++ mssql_custom_query/mssql_custom_query.ps1
@@ -1,14 +1,7 @@
 param([string]$SQLServer , [string]$SQLDBName , [string]$query , [string]$sqlusername , [string]$sqlpassword)
 $version = 1 #If any changes in the plugin metrics, increment the plugin version here. 
-<#$displayname = "MSSQL Query Result check" #>
 $heartbeat = "true"
 $output = @{}
-<##########USer Input #######
-$SQLServer = "SQLEXPRESS" #use Server\Instance for named SQL instances!
-$SQLDBName = "master"
-$sqlusername = "sa"
-$sqlpassword = "Password"
-$query =  'select * from sys.assemblies'#>
 #incase of sql server authentication
 $sqlserverauthetication = $false # make it $false for windows authentication
 Function Get-Data()
@@ -17,10 +10,15 @@ Function Get-Data()
         $units = @{}
         try{
             $results=Invoke-Sqlcmd -ConnectionString "Data Source=$SQLServer; User Id=$sqlusername; Password =$sqlpassword" -Query "$query"
+            $resultlen = $results | Measure-Object | select -ExpandProperty Count
             if($results)
             {
                 $isConnectionSuccessful = "Yes"
                 $isOutputReturned = "Yes"
+                if($resultlen -gt 1)
+                {
+                    $results=$results[0]
+                }
                 for($collen=0; $collen -lt $results.Table.Columns.Count; $collen=$collen+1)
                 {
                     
