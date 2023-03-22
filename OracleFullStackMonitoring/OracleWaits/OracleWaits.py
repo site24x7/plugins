b04c3e31c11b239db3618f3d5b53bb9afab169e2
Suraj D <104214106+SurajDevAnand@users.noreply.github.com>
2023-03-22 16:09:37 +0530

    Update OracleWaits.py

--- OracleFullStackMonitoring/OracleWaits/OracleWaits.py
+++ OracleFullStackMonitoring/OracleWaits/OracleWaits.py
@@ -67,7 +67,7 @@ class oracle:
 
         try:
             try:
-                conn = cx_Oracle.connect(self.username,self.password,self.hostname+':'+str(self.port)+'/'+self.sid)
+                conn = cx_Oracle.connect(user=self.username, password=self.password, dsn=f"{self.hostname}:{self.port}/{self.sid}")
                 c = conn.cursor()
             except Exception as e:
                 self.maindata['status']=0
@@ -128,7 +128,7 @@ if __name__=="__main__":
     parser.add_argument('--username', help='username for oracle',default=username)
     parser.add_argument('--password', help='password for oracle',default=password)
 
-    parser.add_argument('oracle_home',help='oracle home path',default=oracle_home)
+    parser.add_argument('--oracle_home',help='oracle home path',default=oracle_home)
 
 
     parser.add_argument('--logs_enabled', help='enable log collection for this plugin application',default="False")
