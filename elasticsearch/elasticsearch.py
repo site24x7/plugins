a0fdeb881f573fb87494ad0d242f732a8a44a296
Suraj D <104214106+SurajDevAnand@users.noreply.github.com>
2023-12-07 14:25:25 +0530

    Update elasticsearch.py

--- elasticsearch/elasticsearch.py
+++ elasticsearch/elasticsearch.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-
-
-# Other Modules
-import json, os,sys
+import json
 import time
+import os
+import urllib3
+urllib3.disable_warnings()
 
-
-# URL Modules
-from urllib.error import URLError, HTTPError
-from http.client import InvalidURL
-import requests
-from requests.auth import HTTPBasicAuth 
-
-
-
-PLUGIN_VERSION = "1"
-HEARTBEAT =True
+PLUGIN_VERSION=1
+HEARTBEAT=True
 METRICS_UNITS = { 'JVM garbage collector old generation time':'ms',
                   'Average JVM memory usage in garbage collector(%)' : '%',
                   'CPU used (%)':'%',
@@ -29,40 +20,16 @@ METRICS_UNITS = { 'JVM garbage collector old generation time':'ms',
 
                 }
 
+counterFilePath="counter.json"
 
-# Credentials
-HOST='localhost'
-PORT='9200'
-USERNAME = None
-PASSWORD = None
-NODE = None # Name of the node
-os.environ['NO_PROXY'] = 'localhost'
-CAFILE= '/opt/cert/certs/ca/ca.crt' # Add the crt file which was used in your elasticsearch
-
-counterFilePath = '/opt/site24x7/monagent/plugins/elasticsearch_monitoring/counter.json'
-
-
-custom_metrics=["Status of the node", "Node Availability"]
-
-standard_metrics=[
-
-    'disk_reads',
-    'disk_writes',
-    'query_time_in_millis',
-    'query_total',
-    'fetch_time',
-    'jvm_gc_old_coll_count',
-    'jvm_gc_old_coll_time',
-    'jvm_mem_pool_old_used_perc'
-]
-
+#Metrics
 standard_metrics2={
 
     "Queries hit count":("indices","query_cache","hit_count"),
     "Query cache memory size":("indices","query_cache","memory_size_in_bytes"),
     "Query cache miss count":("indices","query_cache","miss_count"),
     "Request cache hit count":("indices","request_cache","hit_count"),
-    "Number of evictions":("indices","request_cache","evictions"), #Done
+    "Number of evictions":("indices","request_cache","evictions"),
     "Request cache memory size":("indices","request_cache","memory_size_in_bytes")
 
 }
@@ -136,39 +103,31 @@ jvm_metrics={
 
 
 
-class ElasticSearch():
+class esk:
+
+    def __init__(self,args):
+        
+        self.maindata={}
+        self.maindata['plugin_version'] = PLUGIN_VERSION
+        self.maindata['heartbeat_required']=HEARTBEAT
+        self.maindata['units']=METRICS_UNITS
+
+        self.hostname=args.hostname
+        self.port=args.port
+        self.nodename=args.nodename
+        self.username=args.username
+        self.password=args.password
+        self.ssl_option=args.ssl_option
+        self.cafile=args.cafile
+
+        if self.ssl_option.lower()=="true":
+            self.url="https://"+self.hostname+":"+str(self.port)
+        elif self.ssl_option.lower()=="false":
+            self.url="http://"+self.hostname+":"+str(self.port)
 
 
-    def __init__(self,username,password,host,port,nodename,ssl,logs_enabled,log_type_name,log_file_path):
-
-        # Credentials to login
-        self.Maindata={}
-        self.username=username
-        self.password=password
-        self.host=host
-        self.port=port
-        self.nodename=nodename
         self.dictCounterValues = {}
         self.loadCounterValues()
-        self._url= "http://"+self.host+":"+self.port
-        self.ssl=ssl
-        self.logsenabled=logs_enabled
-        self.logtypename=log_type_name
-        self.logfilepath=log_file_path
-
-        applog={}
-        
-        if(self.logsenabled in ['True', 'true', '1']):
-            applog["logs_enabled"]=True
-            applog["log_type_name"]=self.logtypename
-            applog["log_file_path"]=self.logfilepath
-        else:
-            applog["logs_enabled"]=False
-            
-        self.Maindata['applog'] = applog
-
-
-
 
     def loadCounterValues(self):
         file_obj = None
@@ -182,76 +141,45 @@ class ElasticSearch():
                 self.dictCounterValues = json.loads(str_counterValues)
             file_obj.close()
 
-
-            
     def updateCounterValues(self,dict_valuesToUpdate):
         if os.path.exists(counterFilePath):
             file_obj = open(counterFilePath,'w')
             file_obj.write(json.dumps(dict_valuesToUpdate))
             file_obj.close()
-    
-
-
-
-
-
-    # Opening the url and parsing the data
-    def _openURL2(self,str_URLsuffix):
-        ssl_choice=None
-
-        if self.ssl=="NO":
-            ssl_choice=False
-            return self.urlhttp(str_URLsuffix,ssl_choice)
         
-        if self.ssl=="YES":
-            ssl_choice=True
-            return self.urlhttp(str_URLsuffix,ssl_choice)
-
-        
-                
-
-
-
-    def urlhttp(self,str_URLsuffix,ssl_choice):
-
-        str_responseData = None
-        url = None
+    def response_data(self, endpoint):
         try:
-            if ssl_choice:
-                url = 'https://'+self.host+':'+self.port + str_URLsuffix
-            if not ssl_choice:
-                url = 'http://'+self.host+':'+self.port + str_URLsuffix
+            import requests
+            from requests.auth import HTTPBasicAuth
 
-
-            response = requests.get(url = url,  auth = HTTPBasicAuth(USERNAME,  PASSWORD), verify=CAFILE) 
-
-            if response.status_code == 200 :
-                byte_responseData = response.content
-                str_responseData = byte_responseData.decode('UTF-8')
-            else:
-               
-                self.dictEsPluginData['status'] = '0'
-                self.dictEsPluginData['msg'] = 'Invalid response after opening URL : ' + str(response.status_code) 
-            
-        except HTTPError as e:
-            self.dictEsPluginData['status'] = '0'
-            self.dictEsPluginData['msg'] ='HTTP Error '+str(e.code)
-        except URLError as e:
-            self.dictEsPluginData['status'] = '0'
-            self.dictEsPluginData['msg'] = 'URL Error '+str(e.reason)
-        except InvalidURL as e:
-            self.dictEsPluginData['status'] = '0'
-            self.dictEsPluginData['msg'] = 'Invalid URL'
+            url=self.url+endpoint
+            r=requests.get(url, timeout=5, auth = HTTPBasicAuth(self.username,  self.password), verify=False)
+            r.raise_for_status()
+        
+        except requests.exceptions.HTTPError as herror:
+            self.maindata['msg']=str(herror)
+            self.maindata['status']=0
+            return False
+        except requests.exceptions.ReadTimeout as errrt: 
+            self.maindata['msg']=f"Timeout Error"
+            self.maindata['status']=0
+            return False
+        except requests.exceptions.ConnectionError as conerr: 
+            self.maindata['msg']=f"Connection Error"
+            self.maindata['status']=0
+            return False
         except Exception as e:
-            self.dictEsPluginData['status'] = '0'
-            self.dictEsPluginData['msg'] = 'Exception while opening stats url in python 2 : ' + str(e)
-        finally:
-            return str_responseData 
-    
-
+            self.maindata['msg']=str(e)
+            self.maindata['status']=0
+            return False
+        return r.content
+        
     def StandardMetrics(self):
-        suffix='/_nodes/stats'
-        searchdata=self._openURL2(suffix)
+        endpoint='/_nodes/stats'
+        searchdata=self.response_data(endpoint)
+        if not searchdata:
+            return self.maindata
+
         searchdata=json.loads(searchdata)        
 
         num_disk_reads = 0
@@ -294,63 +222,50 @@ class ElasticSearch():
                         fetch_time += int(dictNodes[each_node]['indices']['search']['fetch_time_in_millis'])
                     except Exception as e:
                         pass
-
-                    
         try:
-            self.Maindata['disk_write_read_ratio'] = float(num_disk_writes/num_disk_reads)
+            self.maindata['disk_write_read_ratio'] = float(num_disk_writes/num_disk_reads)
         except Exception as e:
             pass
         try:
-            self.Maindata['Fetch to query ratio'] = float(fetch_time/query_time)
+            self.maindata['Fetch to query ratio'] = float(fetch_time/query_time)
         except Exception as e:
             pass
         try:
-            self.Maindata['Latency of the query'] = float(query_time/query_total)
+            self.maindata['Latency of the query'] = float(query_time/query_total)
         except Exception as e:
             pass
         dcTime = time.time()
         if (('jvm_gc_old_coll_count' and 'ct') in self.dictCounterValues):
             try:
-                self.Maindata['JVM garbage collector old generation count'] = jvm_gc_old_coll_count - int(self.dictCounterValues['jvm_gc_old_coll_count'])
+                self.maindata['JVM garbage collector old generation count'] = jvm_gc_old_coll_count - int(self.dictCounterValues['jvm_gc_old_coll_count'])
             except Exception as e:
                 pass
         else:
-            self.Maindata['JVM garbage collector old generation count'] = 0
+            self.maindata['JVM garbage collector old generation count'] = 0
         if (('jvm_gc_old_coll_time' and 'ct') in self.dictCounterValues):
             try:
-                self.Maindata['JVM garbage collector old generation time'] = jvm_gc_old_coll_time - int(self.dictCounterValues['jvm_gc_old_coll_time'])
+                self.maindata['JVM garbage collector old generation time'] = jvm_gc_old_coll_time - int(self.dictCounterValues['jvm_gc_old_coll_time'])
             except Exception as e:
                 pass
         else:
-            self.Maindata['JVM garbage collector old generation time'] = 0
+            self.maindata['JVM garbage collector old generation time'] = 0
+
         self.dictCounterValues['jvm_gc_old_coll_count'] = jvm_gc_old_coll_count
         self.dictCounterValues['jvm_gc_old_coll_time'] = jvm_gc_old_coll_time
         self.dictCounterValues['ct'] = dcTime
         self.updateCounterValues(self.dictCounterValues)
         try:
-            self.Maindata['Average JVM memory usage in garbage collector(%)'] = float(sum(mem_used_perc_list)/len(mem_used_perc_list))
+            self.maindata['Average JVM memory usage in garbage collector(%)'] = float(sum(mem_used_perc_list)/len(mem_used_perc_list))
         except Exception as e:
             pass
-    
-    def MetricCollector(self):
-        
-        self.StandardMetrics()
-        self.parseMetrics(standard_metrics2)
-        self.parseMetrics(os_metrics)
-        self.parseMetrics(search_performance_metrics)
-        self.parseMetrics(index_performance_metrics)
-        self.parseMetrics(http_connection_metrics)
-        self.parseMetrics(unsuccessful_get_metrics)
-        self.parseMetrics(jvm_metrics)
-        self.ClusterHealthMetrics()
 
-        return self.Maindata
-        
     def parseMetrics(self,datapath):
 
         try:
-            suffix='/_nodes/stats'
-            searchdata=self._openURL2(suffix)
+            endpoint='/_nodes/stats'
+            searchdata=self.response_data(endpoint)
+            if not searchdata:
+                return self.maindata
             searchdata=json.loads(searchdata)
             nodes=searchdata['nodes'].keys()
             nodedata=searchdata['nodes']
@@ -361,77 +276,68 @@ class ElasticSearch():
 
                 if searchdata['nodes'][node]['name']==self.nodename:
                     is_node_present=True
-                    self.Maindata['Status of the node']=1
-                    self.Maindata["Node Availability"]="Available"
+                    self.maindata['Status of the node']=1
+                    self.maindata["Node Availability"]="Available"
                     nodedata =searchdata['nodes'][node]
 
                     for key1 in datapath:
                         resultback=nodedata
                         for key2 in datapath[key1]:
                             resultback=resultback[key2]
-                        self.Maindata[key1]=resultback
-
+                        self.maindata[key1]=resultback
 
             if not is_node_present:
                 for key1 in datapath:
-                        self.Maindata[key1]=0
+                        self.maindata[key1]=0
 
-                self.Maindata['Status of the node']=0
-                self.Maindata["Node Availability"]="Node not Found"
+                self.maindata['Status of the node']=0
+                self.maindata["Node Availability"]="Node not Found"
                 pass
 
-
         except Exception as e:
-                self.Maindata['msg']=str(e)        
+                self.maindata['msg']=str(e)        
 
 
 
-    def ClusterHealthMetrics(self):
-        try:
-            suffix='/_cluster/health'
-            searchdata=self._openURL2(suffix)
-            searchdata=json.loads(searchdata)
-            result=searchdata
-            
-            for key1 in cluster_health_node_availability_metrics:
-                self.Maindata[key1]=result[cluster_health_node_availability_metrics[key1]]
 
-        except Exception as e:
-            self.Maindata['msg']=str(e)
+    def metriccollector(self):
+        self.StandardMetrics()
+        self.parseMetrics(standard_metrics2)
+        self.parseMetrics(os_metrics)
+        self.parseMetrics(search_performance_metrics)
+        self.parseMetrics(index_performance_metrics)
+        self.parseMetrics(http_connection_metrics)
+        self.parseMetrics(unsuccessful_get_metrics)
+        self.parseMetrics(jvm_metrics)
+        return self.maindata
 
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
+
+    hostname="localhost"
+    port="9200"
+    nodename="test-node"
+    username=None
+    password=None
+    ssl_option="False"
+    cafile=None
+
+
 
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', help='Host to be monitored',nargs='?', default=HOST)
-    parser.add_argument('--port', help='port number', type=int,  nargs='?', default=PORT)
-    parser.add_argument('--node_name', help='Node name to be monitored', nargs='?', default=NODE)
-    parser.add_argument('--username', help='user name of the elasticsearch', nargs='?', default=USERNAME)
-    parser.add_argument('--password', help='password of the elasticsearch', nargs='?', default=PASSWORD)
-    parser.add_argument('--sslpath', help='elasticsearch ssl path', nargs='?', default=CAFILE)
-    parser.add_argument('--ssl', help='ssl option', nargs='?', default="NO")
-    parser.add_argument('--logs_enabled', help='enable log collection for this plugin application',default="False")
-    parser.add_argument('--log_type_name', help='Display name of the log type', nargs='?', default=None)
-    parser.add_argument('--log_file_path', help='list of comma separated log file paths', nargs='?', default=None)
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--hostname', help='Host to be monitored',nargs='?', default=hostname)
+    parser.add_argument('--port',     help='Port number', type=int,  nargs='?', default=port)
+    parser.add_argument('--nodename', help='Node name to be monitored', nargs='?', default=nodename)
+    parser.add_argument('--username', help='Username of the Elasticsearch', nargs='?', default=username)
+    parser.add_argument('--password', help='Password of the Elasticsearch', nargs='?', default=password)
+    parser.add_argument('--ssl_option', help='Option for Elasticsearch ssl', nargs='?', default=ssl_option)
+    parser.add_argument('--cafile', help='cafile for the Elasticsearch', nargs='?', default=cafile)
+
+    args=parser.parse_args()
+    obj=esk(args)
 
 
-    args = parser.parse_args()
-    host_name=args.host
-    port=str(args.port)
-    node_name=args.node_name    
-    username=args.username
-    password=args.password 
-    sslpath=args.sslpath
-    ssl=args.ssl
-    logs_enabled = args.logs_enabled
-    log_type_name = args.log_type_name
-    log_file_path = args.log_file_path
-    
-
-    esk=ElasticSearch(username,password,host_name,port,node_name,ssl,logs_enabled,log_type_name,log_file_path)
-    result=esk.MetricCollector()
-    result["heartbeat_required"]=HEARTBEAT
-    result["plugin_version"]=PLUGIN_VERSION
-    result['units']=METRICS_UNITS
-    print(json.dumps(result,indent=4))
+    result=obj.metriccollector()
+    print(json.dumps(result,indent=True))
