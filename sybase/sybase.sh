381eadddc24157b9b4680b01ec2811f4d57f2bcb
Suraj D <104214106+SurajDevAnand@users.noreply.github.com>
2024-03-14 11:52:17 +0530

    Add files via upload

---
+++ sybase/sybase.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set echo off
+
+PLUGIN_VERSION="1"
+HEARTBEAT_REQUIRED="true"
+
+
+HOST=""
+PORT=""
+USERNAME=""
+PASSWORD=""
+JAVA_HOME="/usr/bin"
+
+PLUGIN_FOLDER_NAME="sybase"
+
+PLUGIN_PATH="/opt/site24x7/monagent/plugins/"$PLUGIN_FOLDER_NAME
+export CLASS_PATH=$PLUGIN_PATH/json-20140107.jar:$PLUGIN_PATH/jconn4.jar
+
+$JAVA_HOME/javac -cp $CLASS_PATH -d $PLUGIN_PATH $PLUGIN_PATH"/sybase.java"
+data=$($JAVA_HOME/java -cp $CLASS_PATH:$PLUGIN_PATH "sybase" $PLUGIN_VERSION $HEARTBEAT_REQUIRED $HOST $PORT $USERNAME $PASSWORD)
+
+echo "$data"
