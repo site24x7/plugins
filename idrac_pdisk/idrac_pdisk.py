1eaba83460089bb3a47c814f14785906cbe9df80
Manikandan-22230 <manikandan.rengasamy@zohocorp.com>
2024-09-09 16:22:42 +0530

    Update idrac_pdisk.py

--- idrac_pdisk/idrac_pdisk.py
+++ idrac_pdisk/idrac_pdisk.py
@@ -15,19 +15,16 @@ import argparse
 ### Tested in Ubuntu
 ### Tested for snmp version 2c
 
-
 OIDREPLACE = "1.3.6.1.4.1"
-SMISTR="SNMPv2-SMI::enterprises"
-ISOSTR="iso.3.6.1.4.1" 
-
+SMISTR = "SNMPv2-SMI::enterprises"
+ISOSTR = "iso.3.6.1.4.1"
 
 ### OIDS for Getting Physical Disk Details
-OIDS = {'pdisk' : ['1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1']}
+OIDS = {'pdisk': ['1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1']}
 ### OID Attributes
-hardware = {'pdisk' : ['1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.2','1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.4','1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.11','1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.35']}
+hardware = {'pdisk': ['1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.2', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.4', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.24', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.11', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.35', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.17', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.19', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.52', '1.3.6.1.4.1.674.10892.5.5.1.20.130.4.1.58']}
 ### Output Keys and their units
-names = {'pdisk' : ['name','state',{'size': 'MB'},'type']}
-
+names = {'pdisk': ['name', 'State', 'ComponentStatus', {'Size': 'MB'}, 'Type', {'DiskUsedSpaceInMB': 'MB'}, {'DiskFreeSpaceInMB': 'MB'}, 'DiskSecurityStatus', {'DiskBlockSizeInBytes': 'Bytes'}]}
 
 class HardwareParser:
     def __init__(self, hostname, snmp_version, snmp_community_str, mib_location):
@@ -41,18 +38,16 @@ class HardwareParser:
         self.pattern = ''
         
     def getData(self):
-        output_data = {}
-        output_data['data'] = {}
-        output_data['units'] = {}
+        output_data = {'data': {}, 'units': {}}
         for _ in OIDS:
             self.hardware = _
             self.oids = OIDS[self.hardware]
             
             for _ in self.oids:
                 try:
-                    ### SNMPUtil module is used to get the snmp output for the input OIDS
-                    snmpdata = SNMPUtil.SNMPPARSER('snmpwalk',self.hostname, self.snmp_version, self.snmp_community_str,_, self.mib_location, hardware[self.hardware])
-                    ### get Raw SNMP Output as a dict
+                    ### SNMPUtil module is used to get the SNMP output for the input OIDS
+                    snmpdata = SNMPUtil.SNMPPARSER('snmpwalk', self.hostname, self.snmp_version, self.snmp_community_str, _, self.mib_location, hardware[self.hardware])
+                    ### Get Raw SNMP Output as a dict
                     self.snmp_data = snmpdata.getRawData()
                     ### Method to parse the SNMP command output data
                     output_data = self.parseSNMPData(output_data)
@@ -61,24 +56,26 @@ class HardwareParser:
                 
         return output_data
     
-    
     ### Method to parse the SNMP command output data
-    def parseSNMPData(self,output_data):
-        jsondata = output_data['data'] 
-        unitdata = output_data['units'] 
+    def parseSNMPData(self, output_data):
+        jsondata = output_data['data']
+        unitdata = output_data['units']
+        
+        disks = []
+        online_count = 0
+        offline_count = 0
         
         for _ in self.snmp_data:
-            if ( not _.startswith(OIDREPLACE) and _.startswith(SMISTR) ):
+            if not _.startswith(OIDREPLACE) and _.startswith(SMISTR):
                 _ = _.replace(SMISTR, OIDREPLACE)
-            elif ( not _.startswith(OIDREPLACE) and _.startswith(ISOSTR) ):
+            elif not _.startswith(OIDREPLACE) and _.startswith(ISOSTR):
                 _ = _.replace(ISOSTR, OIDREPLACE)
-            #print(_)
             
-            for index, __ in enumerate(hardware[self.hardware]) :
-                if __ in _:        
-                    _ = _.replace('\n','').replace('\r','').replace('"','')
+            for index, __ in enumerate(hardware[self.hardware]):
+                if __ in _:
+                    _ = _.replace('\n', '').replace('\r', '').replace('"', '')
                     name = _.split(' ')[0]
-                    elementname = name[len(name)-1]
+                    elementname = name[len(name) - 1]
 
                     l = _.split(' ')
                     l.pop(0)
@@ -86,31 +83,59 @@ class HardwareParser:
                     
                     if ':' in value:
                         val = value.split(':')[1:] 
-                        value = val[len(val)-1]
-                   
+                        value = val[len(val) - 1]
+                
                     elem = names[self.hardware][index]
                     
-                    attribute = '' # Attribute Name
-                    unit = ''      # Attribute Value
+                    attribute = ''
+                    unit = ''
                     
-                    if type(elem) is str: # Attributes with no units specified
+                    if type(elem) is str:
                         attribute = elem
-                    elif type(elem) is dict: # Attributes with units
+                    elif type(elem) is dict:
                         attribute = list(elem.keys())[0]
                         unit = elem[list(elem.keys())[0]]
                     
-                    key = (attribute +'_'+elementname).replace(' ','')
+                    key = (attribute + '_' + elementname).replace(' ', '')
                     jsondata[key] = value
                     
-                    if unit!='':
+                    if unit != '':
                         unitdata[key] = unit 
-                 
-        output_data['data'] = jsondata
-        output_data['units'] = unitdata
+
+        # Aggregating data into disk list and counting states
+        for i in range(1, len(jsondata) // len(names[self.hardware]) + 1):
+            disk = {
+                'name': f'p_disk_{jsondata.get(f"name_{i}", "")}',
+                'State': jsondata.get(f'State_{i}', ''),
+                'ComponentStatus': jsondata.get(f'ComponentStatus_{i}', ''),
+                'Size': jsondata.get(f'Size_{i}', ''),
+                'DiskUsedSpaceInMB': jsondata.get(f'DiskUsedSpaceInMB_{i}', ''),
+                'DiskFreeSpaceInMB': jsondata.get(f'DiskFreeSpaceInMB_{i}', ''),
+                'Type': jsondata.get(f'Type_{i}', ''),
+                'DiskSecurityStatus': jsondata.get(f'DiskSecurityStatus_{i}', ''),
+                'DiskBlockSizeInBytes': jsondata.get(f'DiskBlockSizeInBytes_{i}', '')
+            }
+            if disk['name']:
+                disks.append(disk)
+                if disk['ComponentStatus'] == '3':
+                    online_count += 1
+                else:
+                    offline_count += 1
+        
+        units = {}
+        for item in names[self.hardware]:
+            if isinstance(item, dict):
+                attribute = list(item.keys())[0]
+                unit = item[attribute]
+                units[attribute] = unit
+
+        output_data['data'] = {'disk': disks}
+        output_data['units'] = {'disk': units}
+        
+        output_data['Disks Online'] = online_count
+        output_data['Disks Offline'] = offline_count
         
         return output_data
-
-
 
 if __name__ == '__main__':
     
@@ -130,10 +155,16 @@ if __name__ == '__main__':
         output = parser.getData()
         result = output['data']
         result['units'] = output['units']
+        
+        total_p_disks = len(result.get('disk', []))
+        result['Total Physical Disks'] = total_p_disks
+        
+        result['Pdisk Plugin Status'] = "ok"
+        result['Disks Online'] = output.get('Disks Online', 0)
+        result['Disks Offline'] = output.get('Disks Offline', 0)
     except Exception as e:
         result['msg'] = str(e)
     
     result['plugin_version'] = args.plugin_version
     result['heartbeat_required'] = args.heartbeat_required
     print(json.dumps(result))
-    
