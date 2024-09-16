361aa515b9637a99da233fcdf2a3cb01eaad5b67
Manikandan-22230 <manikandan.rengasamy@zohocorp.com>
2024-09-16 12:33:07 +0530

    Update idrac_vdisk.py

--- idrac_vdisk/idrac_vdisk.py
+++ idrac_vdisk/idrac_vdisk.py
@@ -15,18 +15,16 @@ import argparse
 ### Tested in Ubuntu
 ### Tested for snmp version 2c
 
-
 OIDREPLACE = "1.3.6.1.4.1"
-SMISTR="SNMPv2-SMI::enterprises"
-ISOSTR="iso.3.6.1.4.1" 
+SMISTR = "SNMPv2-SMI::enterprises"
+ISOSTR = "iso.3.6.1.4.1"
 
-
-### OIDS for Getting Virtual Disk Details
-OIDS = {'vdisk' : ['1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1']}
+### OIDS for Getting virtual Disk Details
+OIDS = {'vdisk': ['1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1']}
 ### OID Attributes
-hardware = {'vdisk' : ['1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.2','1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.4','1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.6','1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.23']}
+hardware = {'vdisk': ['1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.2', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.4', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.20', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.6', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.33', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.38', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.24', '1.3.6.1.4.1.674.10892.5.5.1.20.140.1.1.30']}
 ### Output Keys and their units
-names = {'vdisk' : ['name','state',{'size': 'MB'},'badblocks']}
+names = {'vdisk': ['name', 'State', 'ComponentStatus', {'Size': 'MB'}, 'Type', {'DiskBlockSizeInBytes': 'Bytes'}, 'DiskSecured', 'DiskOperationalState']}
 
 class HardwareParser:
     def __init__(self, hostname, snmp_version, snmp_community_str, mib_location):
@@ -34,81 +32,85 @@ class HardwareParser:
         self.snmp_version = snmp_version
         self.snmp_community_str = snmp_community_str
         self.mib_location = mib_location
-        
-        self.hardware = ''
-        self.oids = ''
-        self.pattern = ''
+        self.hardware = 'vdisk'
+        self.oids = OIDS[self.hardware]
         
     def getData(self):
-        output_data = {}
-        output_data['data'] = {}
-        output_data['units'] = {}
-        for _ in OIDS:
-            self.hardware = _
-            self.oids = OIDS[self.hardware]
-            
-            for _ in self.oids:
-                try:
-                    ### SNMPUtil module is used to get the snmp output for the input OIDS
-                    snmpdata = SNMPUtil.SNMPPARSER('snmpwalk',self.hostname, self.snmp_version, self.snmp_community_str,_, self.mib_location, hardware[self.hardware])
-                    ### get Raw SNMP Output as a dict
-                    self.snmp_data = snmpdata.getRawData()
-                    ### Method to parse the SNMP command output data
-                    output_data = self.parseSNMPData(output_data)
-                except Exception as e:
-                    raise Exception(e)
-                
-        return output_data
-    
-    
-    ### Method to parse the SNMP command output data
-    def parseSNMPData(self,output_data):
-        jsondata = output_data['data'] 
-        unitdata = output_data['units'] 
-        
-        for _ in self.snmp_data:
-            if ( not _.startswith(OIDREPLACE) and _.startswith(SMISTR) ):
-                _ = _.replace(SMISTR, OIDREPLACE)
-            elif ( not _.startswith(OIDREPLACE) and _.startswith(ISOSTR) ):
-                _ = _.replace(ISOSTR, OIDREPLACE)
-            #print(_)
-            
-            for index, __ in enumerate(hardware[self.hardware]) :
-                if __ in _: 
-                    _ = _.replace('\n','').replace('\r','').replace('"','')
-                    name = _.split(' ')[0]
-                    elementname = name[len(name)-1]
+        output_data = {'data': {}, 'units': {}}
+        oid = self.oids[0]
 
-                    l = _.split(' ')
-                    l.pop(0)
-                    value = ' '.join(l)
-                    
+        try:
+            snmpdata = SNMPUtil.SNMPPARSER('snmpwalk', self.hostname, self.snmp_version, self.snmp_community_str, oid, self.mib_location, hardware[self.hardware])
+            self.snmp_data = snmpdata.getRawData()
+            output_data = self.parseSNMPData(output_data)
+        except Exception as e:
+            raise Exception(e)
+            
+        return output_data
+  
+    def parseSNMPData(self, output_data):
+        unitdata = {}
+        disks = []
+        online_count = 0
+        offline_count = 0
+        disk_map = {}
+
+        for snmp_item in self.snmp_data:
+            if not snmp_item.startswith(OIDREPLACE):
+                if snmp_item.startswith(SMISTR):
+                    snmp_item = snmp_item.replace(SMISTR, OIDREPLACE)
+                elif snmp_item.startswith(ISOSTR):
+                    snmp_item = snmp_item.replace(ISOSTR, OIDREPLACE)
+
+            snmp_item = snmp_item.replace('\n', '').replace('\r', '').replace('"', '')
+
+            for index, oid in enumerate(hardware[self.hardware]):
+                if oid in snmp_item:
+                    name = snmp_item.split(' ')[0]
+                    elementname = name.split('.')[-1] 
+                    value = ' '.join(snmp_item.split(' ')[1:]).strip()
+
                     if ':' in value:
-                        val = value.split(':')[1:] 
-                        value = val[len(val)-1]
-                   
-                    elem = names[self.hardware][index]
-                    
-                    attribute = '' # Attribute Name
-                    unit = ''      # Attribute Value
-                    
-                    if type(elem) is str: # Attributes with no units specified
-                        attribute = elem
-                    elif type(elem) is dict: # Attributes with units
-                        attribute = list(elem.keys())[0]
-                        unit = elem[list(elem.keys())[0]]
-                        
-                    key = (attribute +'_'+elementname).replace(' ','')
-                    jsondata[key] = value
-                    
-                    if unit!='':
-                        unitdata[key] = unit 
-                 
-        output_data['data'] = jsondata
-        output_data['units'] = unitdata
-        
-        return output_data
+                        value = value.split(':')[-1].strip()
 
+                    elem = names[self.hardware][index]
+                    attribute = elem if isinstance(elem, str) else list(elem.keys())[0]
+
+                    if elementname not in disk_map:
+                        disk_map[elementname] = {}
+
+                    disk_map[elementname][attribute] = value
+
+        unitdata = {
+        'disk': {
+            'Size': 'MB',
+            'DiskBlockSizeInBytes': 'Bytes'
+        }
+    }
+        for elementname, disk_data in disk_map.items():
+            disk = {
+                'name': f'v_disk_{disk_data.get("name", "")}',
+                'State': disk_data.get('State', ''),
+                'ComponentStatus': disk_data.get('ComponentStatus', ''),
+                'Size': disk_data.get('Size', ''),
+                'DiskBlockSizeInBytes': disk_data.get('DiskBlockSizeInBytes', ''),
+                'DiskSecured': disk_data.get('DiskSecured', ''),
+                'DiskOperationalState': disk_data.get('DiskOperationalState', '')
+            }
+
+            if disk['ComponentStatus'] == '3':
+                online_count += 1
+            else:
+                offline_count += 1
+
+            disks.append(disk)
+
+        output_data['data']['disk'] = disks
+        output_data['units'] = unitdata
+        output_data['Disks Online'] = online_count
+        output_data['Disks Offline'] = offline_count
+
+        return output_data
 
 
 if __name__ == '__main__':
@@ -129,9 +131,14 @@ if __name__ == '__main__':
         output = parser.getData()
         result = output['data']
         result['units'] = output['units']
+        total_v_disks = len(result.get('disk', []))
+        result['Total virtual Disks'] = total_v_disks
+        result['Vdisk Plugin Status'] = "ok"
+        result['Disks Online'] = output.get('Disks Online', 0)
+        result['Disks Offline'] = output.get('Disks Offline', 0)
     except Exception as e:
         result['msg'] = str(e)
     
     result['plugin_version'] = args.plugin_version
     result['heartbeat_required'] = args.heartbeat_required
-    print(json.dumps(result))
+    print(json.dumps(result, indent=4))
