9ac955726bd983176f67ff5bcd0decaf7e950bec
Manikandan-22230 <manikandan.rengasamy@zohocorp.com>
2024-12-24 17:10:49 +0530

    Update internet_speed_check.py

--- internet_speed_check/internet_speed_check.py
+++ internet_speed_check/internet_speed_check.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import json
-import speedtest
+import os
+import sys
 
-#if any impacting changes to this plugin kindly increment the plugin version here.
 PLUGIN_VERSION = "1"
 
-#Setting this to true will alert you when there is a communication problem while posting plugin data to server
-HEARTBEAT="true"
+HEARTBEAT = "true"
 
 plugin_rs = {}
-metric_units = {'ping':'ms','download':'Mbps','upload':'Mbps'}
+metric_units = {'ping': 'ms', 'download': 'Mbps', 'upload': 'Mbps'}
 plugin_rs['plugin_version'] = PLUGIN_VERSION
 plugin_rs['heartbeat_required'] = HEARTBEAT
 
 try:
+    speedtest_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "speedtest-cli")
+
+    sys.path.insert(0, speedtest_path)
+
+    import speedtest
+
     threads = None
     servers = []
     s = speedtest.Speedtest()
@@ -21,12 +26,13 @@ try:
     s.get_best_server()
     s.download(threads=threads)
     s.upload(threads=threads)
-    s.results.share()
     results_dict = s.results.dict()
-    plugin_rs["upload"] = round((results_dict["upload"]/1024)/1024)
-    plugin_rs["download"] = round((results_dict["download"]/1024)/1024)
+
+    plugin_rs["upload"] = round((results_dict["upload"] / 1024) / 1024, 2)
+    plugin_rs["download"] = round((results_dict["download"] / 1024) / 1024, 2)
     plugin_rs["ping"] = results_dict["ping"]
     plugin_rs['units'] = metric_units
+
 except Exception as e:
     plugin_rs['status'] = 0
     plugin_rs['msg'] = str(e)
