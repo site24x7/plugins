cbe5ac7a2cd8057ad47562fc0b0fe36389243170
Manikandan-22230 <manikandan.rengasamy@zohocorp.com>
2025-02-03 16:44:43 +0530

    Update jvm.sh

--- jvm/jvm.sh
+++ jvm/jvm.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin
-PLUGIN_FOLDER="jvx"
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/bin
+PLUGIN_FOLDER="jvm"
 PLUGIN_PATH="/opt/site24x7/monagent/plugins/jvm/"
+host="127.0.0.1"   
+port=7199
+plugin_version="5" 
+heartbeat_required="true"  
 $JAVA_HOME/javac -d $PLUGIN_PATH $PLUGIN_PATH/JVMMonitoring.java
-data=$($JAVA_HOME/java -cp $PLUGIN_PATH JVMMonitoring)
+data=$($JAVA_HOME/java -cp $PLUGIN_PATH JVMMonitoring "$host" "$port" "$plugin_version" "$heartbeat_required")
 echo "$data"
