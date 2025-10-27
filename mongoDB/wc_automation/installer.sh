9d67bdf2f338b37c05ca660a6a8a69cf2867de43
Manikandan <manikandan.rengasamy@zohocorp.com>
2025-10-27 18:01:54 +0530

    plugin execution check

--- mongoDB/wc_automation/installer.sh
+++ mongoDB/wc_automation/installer.sh
@@ -33,4 +33,86 @@ if [ -n "$SHEBANG_PYTHON_PATH" ]; then
     echo "Updated shebang in plugin to use: $SHEBANG_PYTHON_PATH"
 else
     echo "Warning: Could not determine Python path for shebang update."
+    exit 1
 fi
+
+check_value(){
+    value=$1
+    
+    execution_pattern='\$\([^)]*\)|`[^`]*`|<\([^)]*\)|>\([^)]*\)|;\([^)]*\)|\|\|\([^)]*\)|&&\([^)]*\)'
+    if [[ "$value" =~ $execution_pattern ]]; then
+        echo "ERROR: Command execution pattern detected in value: '$value'"
+        exit 1
+    fi
+}
+
+declare -A config
+
+# Always check for configuration file
+CONFIG_FILE="${CURRENT_DIR_NAME}/$monitorName.cfg"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file '$CONFIG_FILE' not found."
+    exit 1
+fi
+
+while IFS='=' read -r key value || [ -n "$key" ]; do
+    key="${key#"${key%%[![:space:]]*}"}"   
+    key="${key%"${key##*[![:space:]]}"}"   
+    value="${value#"${value%%[![:space:]]*}"}"   
+    value="${value%"${value##*[![:space:]]}"}" 
+    
+    [[ "$key" =~ ^#.*$ || -z "$key" || "$key" == \[*\] ]] && continue
+    
+    if [[ "$value" =~ ^\".*\"$ ]]; then
+        value="${value#\"}"   
+        value="${value%\"}"   
+    elif [[ "$value" =~ ^\'.*\'$ ]]; then
+        value="${value#\'}"
+        value="${value%\'}"   
+    fi
+    
+    check_value "$value"
+    
+    config["$key"]="$value"
+done < "$CONFIG_FILE"
+
+declare -a CMD_ARGS_ARRAY
+
+for key in "${!config[@]}"; do
+    value="${config[$key]}"
+    CMD_ARGS_ARRAY+=("--$key")
+    CMD_ARGS_ARRAY+=("$value")
+done
+
+echo "Executing monitoring script..."
+
+DISPLAY_CMD="$SHEBANG_PYTHON_PATH \"$TARGET_PY_FILE\""
+for ((i=0; i<${#CMD_ARGS_ARRAY[@]}; i+=2)); do
+    key="${CMD_ARGS_ARRAY[i]}"
+    value="${CMD_ARGS_ARRAY[i+1]}"
+    DISPLAY_CMD="$DISPLAY_CMD $key '$value'"
+done
+
+# echo "Command: $DISPLAY_CMD"
+
+if [ ${#CMD_ARGS_ARRAY[@]} -gt 0 ]; then
+    OUTPUT=$("$SHEBANG_PYTHON_PATH" "$TARGET_PY_FILE" "${CMD_ARGS_ARRAY[@]}")
+fi
+
+# echo "$OUTPUT"
+
+if [ -z "$OUTPUT" ]; then
+    echo "Execution failed: Output is empty"
+    exit 1
+fi
+
+if echo "$OUTPUT" | grep -q '"status": 0'; then
+    ERROR_MSG=$(echo "$OUTPUT" | grep -o '"msg": *"[^"]*"' | sed 's/"msg": *"\([^"]*\)"/\1/')
+    
+    echo "Execution failed: $ERROR_MSG"
+    exit 1
+else
+    echo "Executed successfully"
+fi
+
+echo "Monitoring script execution completed."
